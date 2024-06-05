95a795e5a231e571257d5fc9ffef1c8f547bd8dc
Shawn Mclean <shawnrmclean@gmail.com>
2024-06-05 14:11:03 -0500

    remove litefs

--- apps/sovoli.com/tests/setup/global-setup.ts
+++ apps/sovoli.com/tests/setup/global-setup.ts
@@ -1,35 +1,32 @@
-import path from 'node:path'
-import { execaCommand } from 'execa'
-import fsExtra from 'fs-extra'
+// import path from 'node:path'
+// import { execaCommand } from 'execa'
+// import fsExtra from 'fs-extra'
 
-export const BASE_DATABASE_PATH = path.join(
-	process.cwd(),
-	`./tests/prisma/base.db`,
-)
+
 
 export async function setup() {
-	const databaseExists = await fsExtra.pathExists(BASE_DATABASE_PATH)
+	// const databaseExists = await fsExtra.pathExists(BASE_DATABASE_PATH)
 
-	if (databaseExists) {
-		const databaseLastModifiedAt = (await fsExtra.stat(BASE_DATABASE_PATH))
-			.mtime
-		const prismaSchemaLastModifiedAt = (
-			await fsExtra.stat('./prisma/schema.prisma')
-		).mtime
+	// if (databaseExists) {
+	// 	const databaseLastModifiedAt = (await fsExtra.stat(BASE_DATABASE_PATH))
+	// 		.mtime
+	// 	const prismaSchemaLastModifiedAt = (
+	// 		await fsExtra.stat('./prisma/schema.prisma')
+	// 	).mtime
 
-		if (prismaSchemaLastModifiedAt < databaseLastModifiedAt) {
-			return
-		}
-	}
+	// 	if (prismaSchemaLastModifiedAt < databaseLastModifiedAt) {
+	// 		return
+	// 	}
+	// }
 
-	await execaCommand(
-		'npx prisma migrate reset --force --skip-seed --skip-generate',
-		{
-			stdio: 'inherit',
-			env: {
-				...process.env,
-				DATABASE_URL: `${BASE_DATABASE_PATH}`,
-			},
-		},
-	)
+	// await execaCommand(
+	// 	'npx prisma migrate reset --force --skip-seed --skip-generate',
+	// 	{
+	// 		stdio: 'inherit',
+	// 		env: {
+	// 			...process.env,
+	// 			DATABASE_URL: `${BASE_DATABASE_PATH}`,
+	// 		},
+	// 	},
+	// )
 }
