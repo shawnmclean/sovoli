aa2ef2bb9404f71ecbc7e57662f3423ed4dcb888
Shawn Mclean <shawnrmclean@gmail.com>
2024-06-13 16:10:27 -0500

    cleaned up all packages

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, View, Text } from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi!</Text>
+      <Text>Hi</Text>
       <StatusBar style="auto" />
     </View>
   )
