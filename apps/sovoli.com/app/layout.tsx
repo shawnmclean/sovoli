45d17ed5590cbed7d8fc3ef0048ede9b50bb24b2
Shawn Mclean <shawnrmclean@gmail.com>
2024-06-17 17:21:43 -0500

    Add speed insights

--- apps/sovoli.com/app/layout.tsx
+++ apps/sovoli.com/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
