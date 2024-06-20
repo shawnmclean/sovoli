84a28b393c19aa346cfe721b85e4535251e6ff85
Shawn Mclean <shawnrmclean@gmail.com>
2024-06-20 18:57:05 -0500

    downgrade next

--- apps/sovoli.com/next.config.js
+++ apps/sovoli.com/next.config.js
@@ -2,18 +2,22 @@ const { withExpo } = require("@expo/next-adapter");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reanimated (and thus, Moti) doesn't work with strict mode currently...
-  // https://github.com/nandorojo/moti/issues/224
-  // https://github.com/necolas/react-native-web/pull/2330
-  // https://github.com/nandorojo/moti/issues/224
-  // once that gets fixed, set this back to true
   reactStrictMode: true,
-  transpilePackages: [
-    "react-native",
-    "react-native-web",
-    "@sovoli/ui",
-    "nativewind",
-  ],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      // Transform all direct `react-native` imports to `react-native-web`
+      "react-native$": "react-native-web",
+    };
+    config.resolve.extensions = [
+      ".web.js",
+      ".web.jsx",
+      ".web.ts",
+      ".web.tsx",
+      ...config.resolve.extensions,
+    ];
+    return config;
+  },
 };
 
 module.exports = withExpo(nextConfig);
