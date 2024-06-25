e89b8911a79b348b4f963e9365384231eace33b7
Shawn Mclean <shawnrmclean@gmail.com>
2024-06-25 14:26:37 -0500

    move expo into src

---
+++ apps/mobile/src/App.tsx
@@ -0,0 +1,21 @@
+import { Text, View, Alert } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Button } from "@sovoli/ui/components/button";
+
+// import "./styles.css";
+
+export default function Native() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl">Native</Text>
+      <Button
+        onClick={() => {
+          console.log("Pressed!");
+          Alert.alert("Pressed!");
+        }}
+        text="Boop"
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
