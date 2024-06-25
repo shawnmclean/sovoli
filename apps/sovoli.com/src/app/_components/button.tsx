0715e129eb2307721677304ac176c2352bc14004
Shawn Mclean <shawnrmclean@gmail.com>
2024-06-25 16:35:02 -0500

    remove classname

--- apps/sovoli.com/src/app/_components/button.tsx
+++ apps/sovoli.com/src/app/_components/button.tsx
@@ -4,11 +4,7 @@ import { Button } from "@sovoli/ui/components/button";
 
 export function ButtonComponent() {
   return (
-    <Button
-      variant="destructive"
-      className="bg-blue-600"
-      onPress={() => alert("clicked")}
-    >
+    <Button variant="destructive" onPress={() => alert("clicked")}>
       Click me
     </Button>
   );
