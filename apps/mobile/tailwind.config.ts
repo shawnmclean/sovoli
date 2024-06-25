7cc928561e288e5bbb5dd8ad42a4ce11fcb966b8
Shawn Mclean <shawnrmclean@gmail.com>
2024-06-25 14:51:17 -0500

    add tailwind

---
+++ apps/mobile/tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "tailwindcss";
+// @ts-expect-error - no types
+import nativewind from "nativewind/preset";
+
+
+export default {
+  content: ["./src/**/*.{ts,tsx}"],
+  presets: [nativewind],
+} satisfies Config;
