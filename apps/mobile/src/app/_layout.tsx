ff016c0c70a0204c016883cce5336aa87dcdbe9d
Shawn Mclean <shawnrmclean@gmail.com>
2024-06-26 10:01:07 -0500

    switch to expo router

---
+++ apps/mobile/src/app/_layout.tsx
@@ -0,0 +1,30 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "nativewind";
+
+import "../styles.css";
+
+// This is the main layout of the app
+// It wraps your pages with the providers they need
+export default function RootLayout() {
+  const { colorScheme } = useColorScheme();
+  return (
+    <>
+      {/*
+          The Stack component displays the current page.
+          It also allows you to configure your screens 
+        */}
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#f472b6",
+          },
+          contentStyle: {
+            backgroundColor: colorScheme == "dark" ? "#09090B" : "#FFFFFF",
+          },
+        }}
+      />
+      <StatusBar />
+    </>
+  );
+}
---
+++ apps/mobile/src/app/index.tsx
@@ -0,0 +1,22 @@
+import { Alert, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Stack } from "expo-router";
+import { Button } from "@sovoli/ui/components/button";
+
+export default function Index() {
+  return (
+    <SafeAreaView className="bg-background">
+      {/* Changes page title visible on the header */}
+      <Stack.Screen options={{ title: "Home Page" }} />
+      <View className="h-full w-full bg-background p-4">
+        <Text className="pb-2 text-center text-5xl font-bold text-foreground">
+          Create <Text className="text-primary">T3</Text> Turbo
+        </Text>
+
+        <Button onPress={() => Alert.alert("Hello!")}>
+          <Text>Click me now!</Text>
+        </Button>
+      </View>
+    </SafeAreaView>
+  );
+}
