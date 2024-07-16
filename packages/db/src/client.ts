990378746ae0c1780a6579ad91162bd28a4f727b
Shawn Mclean <shawnrmclean@gmail.com>
2024-07-16 18:52:44 -0500

    set db adapter to vercel pg

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,7 +1,6 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { sql } from "@vercel/postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
 
 import * as schema from "./schema";
 
-const client = postgres(process.env.POSTGRES_URL!);
-export const db = drizzle(client, { schema });
+export const db = drizzle(sql, { schema });
