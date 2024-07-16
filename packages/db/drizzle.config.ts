f7f905e78db6b76030e04db89e8f1bb8929f77a0
Shawn Mclean <shawnrmclean@gmail.com>
2024-07-16 17:34:28 -0500

    switch to regular postgres

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -4,11 +4,9 @@ if (!process.env.POSTGRES_URL) {
   throw new Error("Missing POSTGRES_URL");
 }
 
-const nonPoolingUrl = process.env.POSTGRES_URL.replace(":6543", ":5432");
-
 export default defineConfig({
   schema: "./src/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
-  dbCredentials: { url: nonPoolingUrl },
+  dbCredentials: { url: process.env.POSTGRES_URL },
 });
--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,6 +1,6 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 import * as schema from "./schema";
 
-export const db = drizzle(sql, { schema });
+export const db = drizzle(postgres, { schema });
