828d329a04f4c0d02b7769b5970e9fb53b1ed0fe
Shawn Mclean <shawnrmclean@gmail.com>
2024-07-21 10:01:17 -0500

    dont export handler

--- apps/sovoli.com/src/app/api/v1/[...ts-rest]/route.ts
+++ apps/sovoli.com/src/app/api/v1/[...ts-rest]/route.ts
@@ -1,7 +1,7 @@
 import { router, contract } from "@sovoli/api/rest";
 import { createNextHandler } from "@ts-rest/serverless/next";
 
-export const handler = createNextHandler(contract, router, {
+const handler = createNextHandler(contract, router, {
   handlerType: "app-router",
   basePath: "/api/v1",
   jsonQuery: true,
