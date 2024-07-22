718047c0a78ed73a92752354806d70b1cc54266e
Shawn Mclean <shawnrmclean@gmail.com>
2024-07-22 12:54:23 -0500

    change vercel url to production url to use custom domains

--- apps/sovoli.com/src/env.ts
+++ apps/sovoli.com/src/env.ts
@@ -10,6 +10,7 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "production", "test"])
       .default("development"),
+    VERCEL_PROJECT_PRODUCTION_URL: z.string().optional(),
   },
   /**
    * Specify your server-side environment variables schema here.
@@ -26,12 +27,13 @@ export const env = createEnv({
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
   },
+
   /**
    * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
    */
   experimental__runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-
+    VERCEL_PROJECT_PRODUCTION_URL: process.env.VERCEL_PROJECT_PRODUCTION_URL,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   skipValidation:
