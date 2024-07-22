a947e097e2fb227575339313299a02947debbfa2
Shawn Mclean <shawnrmclean@gmail.com>
2024-07-22 10:08:20 -0500

    Revert "swap from vercel/postgres"
    This reverts commit 85c40e4ed6fd9884f10429b053623c0e461f42f4.

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,12 +1,6 @@
-// import { sql } from "@vercel/postgres";
-// import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
 
 import * as schema from "./schema";
 
-// export const db = drizzle(sql, { schema });
-
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-
-const client = postgres(process.env.POSTGRES_URL!);
-export const db = drizzle(client, { schema });
+export const db = drizzle(sql, { schema });
