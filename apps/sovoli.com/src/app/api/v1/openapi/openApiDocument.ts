5096fe3c03f89df7bbd8f963d534140f85183286
Shawn Mclean <shawnrmclean@gmail.com>
2024-07-22 14:24:56 -0500

    add operation id

--- apps/sovoli.com/src/app/api/v1/openapi/openApiDocument.ts
+++ apps/sovoli.com/src/app/api/v1/openapi/openApiDocument.ts
@@ -2,12 +2,18 @@ import { contract } from "@sovoli/api/rest";
 import { generateOpenApi } from "@ts-rest/open-api";
 import { getBaseUrl } from "~/utils/getBaseUrl";
 
-const openApiDocument = generateOpenApi(contract, {
-  info: {
-    title: "Sovoli API",
-    version: "1.0.0",
+const openApiDocument = generateOpenApi(
+  contract,
+  {
+    info: {
+      title: "Sovoli API",
+      version: "1.0.0",
+    },
   },
-});
+  {
+    setOperationId: true,
+  }
+);
 openApiDocument.servers = [
   {
     url: `${getBaseUrl()}/api/v1`,
