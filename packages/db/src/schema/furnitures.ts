ea6eb5512efa69728518bddd4ae7bada7d34792e
Shawn Mclean <shawnrmclean@gmail.com>
2024-07-23 14:41:52 -0500

    unique constraint on ownerid and furnitureid

--- packages/db/src/schema/furnitures.ts
+++ packages/db/src/schema/furnitures.ts
@@ -1,18 +1,24 @@
 import { relations } from "drizzle-orm";
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { pgTable, unique, uuid, varchar } from "drizzle-orm/pg-core";
 import { users } from "./identity";
 import { myBooks } from "./myBooks";
 import { createSelectSchema } from "drizzle-zod";
 
-export const furnitures = pgTable("furniture", {
-  id: uuid("id").notNull().primaryKey().defaultRandom(),
-  slug: varchar("slug", { length: 255 }).notNull().unique(),
-  name: varchar("name", { length: 255 }).notNull(),
-  description: varchar("description", { length: 255 }),
-  ownerId: uuid("ownerId")
-    .notNull()
-    .references(() => users.id),
-});
+export const furnitures = pgTable(
+  "furniture",
+  {
+    id: uuid("id").notNull().primaryKey().defaultRandom(),
+    slug: varchar("slug", { length: 255 }).notNull(),
+    name: varchar("name", { length: 255 }).notNull(),
+    description: varchar("description", { length: 255 }),
+    ownerId: uuid("ownerId")
+      .notNull()
+      .references(() => users.id),
+  },
+  (table) => ({
+    uniqueName: unique().on(table.ownerId, table.slug),
+  })
+);
 
 export const SelectFurnitureSchema = createSelectSchema(furnitures);
 
@@ -24,15 +30,21 @@ export const furnituresRelations = relations(furnitures, ({ one, many }) => ({
   shelves: many(shelves),
 }));
 
-export const shelves = pgTable("shelf", {
-  id: uuid("id").notNull().primaryKey().defaultRandom(),
-  slug: varchar("slug", { length: 255 }).notNull().unique(),
-  name: varchar("name", { length: 255 }).notNull(),
-  description: varchar("description", { length: 255 }),
-  furnitureId: uuid("furnitureId")
-    .notNull()
-    .references(() => furnitures.id),
-});
+export const shelves = pgTable(
+  "shelf",
+  {
+    id: uuid("id").notNull().primaryKey().defaultRandom(),
+    slug: varchar("slug", { length: 255 }).notNull(),
+    name: varchar("name", { length: 255 }).notNull(),
+    description: varchar("description", { length: 255 }),
+    furnitureId: uuid("furnitureId")
+      .notNull()
+      .references(() => furnitures.id),
+  },
+  (table) => ({
+    uniqueName: unique().on(table.furnitureId, table.slug),
+  })
+);
 
 export const shelvesRelations = relations(shelves, ({ one, many }) => ({
   furniture: one(furnitures, {
