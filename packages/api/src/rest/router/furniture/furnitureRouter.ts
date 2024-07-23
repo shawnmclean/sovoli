b828e65c06c93a919f0d246a037222f6acfdfd0e
Shawn Mclean <shawnrmclean@gmail.com>
2024-07-23 14:28:46 -0500

    furniture filters on username

--- packages/api/src/rest/router/furniture/furnitureRouter.ts
+++ packages/api/src/rest/router/furniture/furnitureRouter.ts
@@ -1,14 +1,21 @@
 import { tsr } from "@ts-rest/serverless/next";
-import { eq } from "@sovoli/db";
-
-import { db, schema } from "@sovoli/db";
+import { db, eq, and, schema, inArray } from "@sovoli/db";
 
 import { furnitureContract } from "./furnitureContract";
 
 export const furnitureRouter = tsr.router(furnitureContract, {
   getFurniture: async ({ params: { username, slug } }) => {
     const furniture = await db.query.furnitures.findFirst({
-      where: eq(schema.furnitures.slug, slug),
+      where: and(
+        eq(schema.furnitures.slug, slug),
+        inArray(
+          schema.furnitures.ownerId,
+          db
+            .select({ id: schema.users.id })
+            .from(schema.users)
+            .where(eq(schema.users.username, username))
+        )
+      ),
     });
 
     if (!furniture)
