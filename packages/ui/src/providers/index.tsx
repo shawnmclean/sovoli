543f2cb694aa3f3f355a98546301a19b40a38828
Shawn Mclean <shawnrmclean@gmail.com>
2024-08-02 11:29:12 -0500

    🎨 setting up gluestack

--- packages/ui/src/providers/index.tsx
+++ packages/ui/src/providers/index.tsx
@@ -1,22 +1,25 @@
 "use client";
 
 // react-native will select .web.tsx for web versions
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import { GluestackUIProvider } from "./gluestack-ui-provider";
 import { StylesProvider } from "./styles-provider";
 import { ThemeProvider } from "./theme-provider";
-import { SafeAreaProvider } from "./safe-area-provider";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export function UiProviders({ children }: { children: React.ReactNode }) {
   return (
     <>
       {/* <SafeAreaProvider> */}
-      <ThemeProvider>
-        <StylesProvider>
-          <GestureHandlerRootView style={{ flex: 1 }}>
-            {children}
-          </GestureHandlerRootView>
-        </StylesProvider>
-      </ThemeProvider>
+      <GluestackUIProvider>
+        <ThemeProvider>
+          <StylesProvider>
+            <GestureHandlerRootView style={{ flex: 1 }}>
+              {children}
+            </GestureHandlerRootView>
+          </StylesProvider>
+        </ThemeProvider>
+      </GluestackUIProvider>
       {/* </SafeAreaProvider> */}
     </>
   );
