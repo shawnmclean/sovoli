5aaff74445d9746ddace6709ed602dd7777c8297
Shawn Mclean <shawnrmclean@gmail.com>
2024-08-06 12:41:20 -0500

    lift trpc to catalog

--- packages/api/src/tsr/root.ts
+++ packages/api/src/tsr/root.ts
@@ -1,9 +1,10 @@
 import { tsr } from "@ts-rest/serverless/next";
+
 import { contract } from "./contract";
-import { userRouter } from "./router/user/userRouter";
 import { furnitureRouter } from "./router/furniture/furnitureRouter";
-import { shelfRouter } from "./router/shelf/shelfRouter";
 import { myBookRouter } from "./router/myBook/myBookRouter";
+import { shelfRouter } from "./router/shelf/shelfRouter";
+import { userRouter } from "./router/user/userRouter";
 
 export const router = tsr.router(contract, {
   ...userRouter,
