beee8e4733b9b8a647c4ddac43edd0c77bc61382
Shawn Mclean <shawnrmclean@gmail.com>
2024-08-08 15:35:56 -0500

    took down user page temporarily

--- apps/sovoli.com/src/app/(profile)/[username]/_components/User.tsx
+++ apps/sovoli.com/src/app/(profile)/[username]/_components/User.tsx
@@ -1,21 +1,19 @@
 "use client";
 
-import { trpc } from "~/api/react";
-import { UserScreen } from "@sovoli/ui/screens/user";
+// import { trpc } from "~/api/react";
+// import { UserScreen } from "@sovoli/ui/screens/user";
 
 export function User({ username }: { username: string }) {
-  const [user] = trpc.user.byUsername.useSuspenseQuery({ username });
+  // const [user] = trpc.user.byUsername.useSuspenseQuery({ username });
 
-  if (!user) {
-    return null;
-  }
+  // if (!user) {
+  //   return null;
+  // }
 
   return (
     <>
-      <h1>
-        {user.username} - {user.id} - {user.name}
-      </h1>
-      <UserScreen username={user.username} />
+      <h1>{username}</h1>
+      {/* <UserScreen username={user.username} /> */}
     </>
   );
 }
--- apps/sovoli.com/src/app/(profile)/[username]/page.tsx
+++ apps/sovoli.com/src/app/(profile)/[username]/page.tsx
@@ -1,15 +1,16 @@
-import { api, HydrateClient } from "~/api/trpc";
-import { User } from "./_components/User";
+// import { api, HydrateClient } from "~/api/trpc";
 import { Suspense } from "react";
 
+import { User } from "./_components/User";
+
 export default function UserPage({ params }: { params: { username: string } }) {
-  void api.user.byUsername.prefetch({ username: params.username });
+  // void api.user.byUsername.prefetch({ username: params.username });
 
   return (
-    <HydrateClient>
-      <Suspense fallback={<div>Loading...</div>}>
-        <User username={params.username} />
-      </Suspense>
-    </HydrateClient>
+    // <HydrateClient>
+    <Suspense fallback={<div>Loading...</div>}>
+      <User username={params.username} />
+    </Suspense>
+    // </HydrateClient>
   );
 }
