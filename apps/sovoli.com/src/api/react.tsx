a7cd239f035a282a07efcf836a8dddf25aac78cb
Shawn Mclean <shawnrmclean@gmail.com>
2024-08-08 13:29:11 -0500

    refactor client

--- apps/sovoli.com/src/api/react.tsx
+++ apps/sovoli.com/src/api/react.tsx
@@ -1,49 +1,49 @@
 "use client";
 
+import type { AppRouter } from "@sovoli/api/trpc";
 import { useState } from "react";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
+import {
+  defaultShouldDehydrateQuery,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+// import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import SuperJSON from "superjson";
 
-import type { AppRouter } from "@sovoli/api/trpc";
+// import { env } from "~/env";
+// import { getBaseUrl } from "~/utils/getBaseUrl";
+// import { getQueryClient } from "./query-client";
 import { tsr } from "./tsr";
-
-import { env } from "~/env";
-import { getBaseUrl } from "~/utils/getBaseUrl";
-import { getQueryClient } from "./query-client";
 
 export const trpc = createTRPCReact<AppRouter>();
 
 export function QueryProviders(props: { children: React.ReactNode }) {
-  const queryClient = getQueryClient();
-
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [
-        loggerLink({
-          enabled: (op) =>
-            env.NODE_ENV === "development" ||
-            (op.direction === "down" && op.result instanceof Error),
-        }),
-        unstable_httpBatchStreamLink({
-          transformer: SuperJSON,
-          url: getBaseUrl() + "/api/trpc",
-          headers() {
-            const headers = new Headers();
-            headers.set("x-trpc-source", "nextjs-react");
-            return headers;
+  const [trpcClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 30 * 1000,
           },
-        }),
-      ],
-    })
+          dehydrate: {
+            serializeData: SuperJSON.serialize,
+            shouldDehydrateQuery: (query) =>
+              defaultShouldDehydrateQuery(query) ||
+              query.state.status === "pending",
+          },
+          hydrate: {
+            deserializeData: SuperJSON.deserialize,
+          },
+        },
+      }),
   );
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <trpc.Provider client={trpcClient} queryClient={queryClient}>
-        <tsr.ReactQueryProvider>{props.children}</tsr.ReactQueryProvider>
-      </trpc.Provider>
+    <QueryClientProvider client={trpcClient}>
+      <tsr.ReactQueryProvider>{props.children}</tsr.ReactQueryProvider>
     </QueryClientProvider>
   );
 }
