3483130933a7f4c1b7c382e149124039c41b3f80
Shawn Mclean <shawnrmclean@gmail.com>
2024-08-13 11:40:23 -0500

    update trigger.dev linter issues

--- packages/api/src/trigger/example.ts
+++ packages/api/src/trigger/example.ts
@@ -1,12 +1,17 @@
-import { task } from "@trigger.dev/sdk/v3";
+import { logger, task, wait } from "@trigger.dev/sdk/v3";
 
 //1. You need to export each task
 export const helloWorld = task({
   //2. Use a unique id for each task
   id: "hello-world",
   //3. The run function is the main function of the task
-  run: async (payload: { message: string }) => {
-    //4. You can write code that runs for a long time here, there are no timeouts
-    console.log(payload.message);
+  run: async ({ message }: { message: string }, { ctx }) => {
+    logger.log(`q: ${message}`, { message, ctx });
+
+    await wait.for({ seconds: 5 });
+
+    return {
+      message: `Hello, ${message}!`,
+    };
   },
 });
