0cb74539743b489fc0d0075fa73952f8ed8fa3cd
Shawn Mclean <shawnrmclean@gmail.com>
2024-08-14 12:34:35 -0500

    clean up get shelf page

---
+++ apps/sovoli.com/src/app/(profile)/[username]/shelves/[slug]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Shelf is not found for the user</h1>
+    </div>
+  );
+}
--- apps/sovoli.com/src/app/(profile)/[username]/shelves/[slug]/page.tsx
+++ apps/sovoli.com/src/app/(profile)/[username]/shelves/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { notFound } from "next/navigation";
 import { ShelfScreen } from "@sovoli/ui/screens/mybooks/shelf";
 
 import { getQueryClientRsc } from "~/api/query-client";
@@ -12,25 +13,40 @@ interface Props {
   searchParams: { page: number | undefined };
 }
 
+async function getShelfBooks({ params, searchParams }: Props) {
+  const client = tsr.initQueryClient(getQueryClientRsc());
+  try {
+    return await client.getShelfBooks.fetchQuery({
+      queryKey: ["username", "slug"],
+      queryData: {
+        params: {
+          username: params.username,
+          slug: params.slug,
+        },
+        query: {
+          page: searchParams.page,
+        },
+      },
+    });
+  } catch (error) {
+    // Type guard to check if error is an object with a 'status' property
+    if (typeof error === "object" && error !== null && "status" in error) {
+      const status = (error as { status?: number }).status; // Safe type assertion
+
+      if (status === 404) return null;
+    }
+  }
+}
+
 export async function generateMetadata({
   params,
   searchParams,
 }: Props): Promise<Metadata> {
-  const client = tsr.initQueryClient(getQueryClientRsc());
-  const { body } = await client.getShelfBooks.fetchQuery({
-    queryKey: ["username", "slug"],
-    queryData: {
-      params: {
-        username: params.username,
-        slug: params.slug,
-      },
-      query: {
-        page: searchParams.page,
-      },
-    },
-  });
+  const response = await getShelfBooks({ params, searchParams });
 
-  const shelf = body.shelf;
+  if (!response) return notFound();
+
+  const shelf = response.body.shelf;
   const coverImage = shelf.images?.[0];
 
   return {
@@ -51,19 +67,9 @@ export async function generateMetadata({
 }
 
 export default async function ShelfPage({ params, searchParams }: Props) {
-  const client = tsr.initQueryClient(getQueryClientRsc());
-  const { body } = await client.getShelfBooks.fetchQuery({
-    queryKey: ["username", "slug"],
-    queryData: {
-      params: {
-        username: params.username,
-        slug: params.slug,
-      },
-      query: {
-        page: searchParams.page,
-      },
-    },
-  });
+  const response = await getShelfBooks({ params, searchParams });
 
-  return <ShelfScreen shelf={body} />;
+  if (!response) return notFound();
+
+  return <ShelfScreen shelf={response.body} />;
 }
