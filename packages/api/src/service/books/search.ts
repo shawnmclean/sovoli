6e68e5a394eaa1ea834de18b095e9cd8a71cdcd1
Shawn Mclean <shawnrmclean@gmail.com>
2024-08-16 12:48:13 -0500

    implement caching for embeddings

--- packages/api/src/service/books/search.ts
+++ packages/api/src/service/books/search.ts
@@ -1,4 +1,6 @@
-import { SelectBookSchema } from "@sovoli/db/schema";
+import type { SelectBookSchema } from "@sovoli/db/schema";
+import { db, inArray, sql } from "@sovoli/db";
+import { embeddingsCache } from "@sovoli/db/schema";
 
 export interface SearchBooksQuery {
   isbn?: string;
@@ -37,22 +39,102 @@ export interface SearchBooksOptions {
 export async function searchBooks(
   options: SearchBooksOptions,
 ): Promise<SearchBooksQueryResult[]> {
+  // separate the queries into the ones that uses ISBN and the ones that use the query
+  const { queryQueries } = options.queries.reduce<{
+    isbnQueries: SearchBooksQuery[];
+    queryQueries: SearchBooksQuery[];
+  }>(
+    (acc, query) => {
+      if (query.isbn) {
+        acc.isbnQueries.push(query);
+      } else if (query.query) {
+        acc.queryQueries.push(query);
+      }
+
+      return acc;
+    },
+    { isbnQueries: [], queryQueries: [] },
+  );
+
+  // TODO: figure out parallelization
+  const queries = queryQueries.map((query) => query.query ?? "");
+  const queriesResults = await searchEmbeddings(queries);
+
+  console.log(">>> searchBooks", { queriesResults });
+
+  // TODO: any missing books should be added to the db
+
   return [];
 }
 
-function searchEmbeddings(query: string): Promise<SelectBookSchema[]> {
+async function searchEmbeddings(
+  queries: string[],
+): Promise<SelectBookSchema[]> {
   // check if the query is in the cache
+  const cachedEmbeddings = await getCachedEmbeddings(queries);
 
+  // get queries that need to be updated
+  const queriesToUpdate = queries.filter((query) => !cachedEmbeddings[query]);
+
+  // if there are queries to update, update the cache
+  if (queriesToUpdate.length > 0) {
+    const embeddings = await getOpenAIEmbeddings(queriesToUpdate);
+
+    const embedData = embeddings.map((embedding, index) => ({
+      // ensure the text never is undefined in the event openAI returns more embeddings than queries
+      text: queriesToUpdate[index] ?? `unknown-${index}`,
+      embedding,
+    }));
+
+    await setCachedEmbedding(embedData);
+  }
+
+  // TODO query the db with the embeddings for the books
   return [];
 }
 
-function getCachedEmbedding(query: string): Promise<number[] | undefined> {
-  // get embeddings from cache
-  console.log(">>> getCachedEmbedding", { query });
-  return undefined;
+async function getCachedEmbeddings(queries: string[]) {
+  const rows = await db
+    .select({
+      text: embeddingsCache.text,
+      openAIEmbedding: embeddingsCache.openAIEmbedding,
+    })
+    .from(embeddingsCache)
+
+    .where(inArray(embeddingsCache.text, queries));
+
+  const embeddings: Record<string, number[] | null> = {};
+  queries.forEach((query) => (embeddings[query] = null));
+
+  rows.forEach((row) => {
+    embeddings[row.text] = row.openAIEmbedding;
+  });
+
+  return embeddings;
 }
 
-function getOpenAIEmbedding(query: string): Promise<number[]> {
-  console.log(">>> getOpenAIEmbedding", { query });
-  return [];
+async function setCachedEmbedding(
+  data: { text: string; embedding: number[] }[],
+) {
+  const insertData = data.map((d) => ({
+    text: d.text,
+    openAIEmbedding: d.embedding,
+  }));
+
+  await db
+    .insert(embeddingsCache)
+    .values(insertData)
+    .onConflictDoUpdate({
+      target: embeddingsCache.text,
+      set: {
+        openAIEmbedding: sql.raw(
+          `excluded.${embeddingsCache.openAIEmbedding.name}`,
+        ),
+      },
+    });
+}
+
+function getOpenAIEmbeddings(queries: string[]): Promise<number[][]> {
+  console.log(">>> getOpenAIEmbedding", { queries });
+  throw new Error("Not implemented");
 }
