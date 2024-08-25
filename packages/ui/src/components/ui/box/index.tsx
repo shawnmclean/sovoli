bdfa36b67f2785027e8e624bdd11991883658d69
Shawn Mclean <shawnrmclean@gmail.com>
2024-08-25 18:24:10 -0500

    fix lint

--- packages/ui/src/components/ui/box/index.tsx
+++ packages/ui/src/components/ui/box/index.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import { View, ViewProps } from 'react-native';
+import { View } from 'react-native';
+import type { ViewProps } from 'react-native';
 
 import type { VariantProps } from '@gluestack-ui/nativewind-utils';
 import { boxStyle } from './styles';
