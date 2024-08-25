7415250c1ed1caf48417c93d52df5caa3f96f8f6
Shawn Mclean <shawnrmclean@gmail.com>
2024-08-25 15:10:26 -0500

    mobile theme fixed

--- packages/ui/src/providers/gluestack-ui-provider/index.tsx
+++ packages/ui/src/providers/gluestack-ui-provider/index.tsx
@@ -1,41 +1,25 @@
 
 import React from 'react';
 import { config } from './config';
-import { ColorSchemeName, useColorScheme, View, ViewProps } from 'react-native';
+import type { ViewProps } from 'react-native';
+import {  View} from 'react-native';
+import { useColorScheme } from '../../hooks/useColorScheme';
 import { OverlayProvider } from '@gluestack-ui/overlay';
 import { ToastProvider } from '@gluestack-ui/toast';
-import { colorScheme as colorSchemeNW } from 'nativewind';
 
-type ModeType = 'light' | 'dark' | 'system';
-
-const getColorSchemeName = (
-  colorScheme: ColorSchemeName,
-  mode: ModeType
-): 'light' | 'dark' => {
-  if (mode === 'system') {
-    return colorScheme ?? 'light';
-  }
-  return mode;
-};
 
 export function GluestackUIProvider({
-  mode = 'light',
   ...props
 }: {
-  mode?: 'light' | 'dark' | 'system';
   children?: React.ReactNode;
   style?: ViewProps['style'];
 }) {
-  const colorScheme = useColorScheme();
-
-  const colorSchemeName = getColorSchemeName(colorScheme, mode);
-
-  colorSchemeNW.set(mode);
+  const {colorScheme} = useColorScheme();
 
   return (
     <View
       style={[
-        config[colorSchemeName],
+        config[colorScheme],
         { flex: 1, height: '100%', width: '100%' },
         props.style,
       ]}
