e1c750067c73ce584fe28bad6d68dda040844eb6
Shawn Mclean <shawnrmclean@gmail.com>
2024-08-26 21:39:02 -0500

    fix linting

--- apps/sovoli.com/src/app/page.tsx
+++ apps/sovoli.com/src/app/page.tsx
@@ -39,7 +39,8 @@ async function SignIn() {
   return (
     <div>
       Logged in as {session.user.name} - {session.user.email} -{" "}
-      {session.user.username}
+      {session.user.username} - {session.user.id}
+      <pre>{JSON.stringify(session, null, 2)}</pre>
     </div>
   );
 }
--- apps/sovoli.com/src/auth.ts
+++ apps/sovoli.com/src/auth.ts
@@ -1,6 +1,12 @@
 import type { DefaultSession } from "next-auth";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@sovoli/db";
+// import {
+//   accounts,
+//   sessions,
+//   users,
+//   verificationTokens,
+// } from "@sovoli/db/schema";
 import NextAuth from "next-auth";
 import Resend from "next-auth/providers/resend";
 
@@ -12,13 +18,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   ],
   adapter: DrizzleAdapter(db),
   callbacks: {
-    session({ session }) {
+    session({ session, user }) {
       // `session.user.address` is now a valid property, and will be type-checked
       // in places like `useSession().data.user` or `auth().user`
       return {
         ...session,
         user: {
           ...session.user,
+          username: user.username,
         },
       };
     },
@@ -30,11 +37,14 @@ declare module "next-auth" {
    * The shape of the user object returned in the OAuth providers' `profile` callback,
    * or the second parameter of the `session` callback, when using a database.
    */
-  interface User {}
+  interface User {
+    username: string;
+  }
   /**
    * The shape of the account object returned in the OAuth providers' `account` callback,
    * Usually contains information about the provider being used, like OAuth tokens (`access_token`, etc).
    */
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface Account {}
   /**
    * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
