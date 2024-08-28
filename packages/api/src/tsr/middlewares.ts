194b19b8e4a21c8ab69251205c346f82d2ab32f1
Shawn Mclean <shawnrmclean@gmail.com>
2024-08-28 14:12:21 -0500

    add comments to middleware

--- packages/api/src/tsr/middlewares.ts
+++ packages/api/src/tsr/middlewares.ts
@@ -2,10 +2,11 @@ import { tsr } from "@ts-rest/serverless/next";
 
 import type { GlobalRequestContext } from "./router/identity/identityRouter";
 
+// Temp until we move the auth into its own package
 function validateToken(authToken: string) {
   return { user: { id: authToken } };
 }
-
+// Temp until we move the auth into its own package
 function auth() {
   return null;
 }
@@ -14,6 +15,7 @@ function auth() {
  * Isomorphic Session getter for API requests
  * - Expo requests will have a session token in the Authorization header
  * - Next.js requests will have a session token in cookies
+ * See: https://github.com/t3-oss/create-t3-turbo/blob/8ca45cd2b06096c14d36a713dce32d7afcb1fed7/packages/api/src/trpc.ts#L22
  */
 const isomorphicGetSession = (headers: Headers) => {
   const authToken = headers.get("Authorization") ?? null;
