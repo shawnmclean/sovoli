efdc50e3c782338495c272328c010b125aaa1f23
Shawn Mclean <shawnrmclean@gmail.com>
2024-08-31 22:55:03 -0500

    tweak safe area

--- packages/ui/src/components/MainLayout/index.tsx
+++ packages/ui/src/components/MainLayout/index.tsx
@@ -1,21 +1,21 @@
 import { Header } from "../Header";
 import { HStack } from "../ui/hstack";
-import { SafeAreaView } from "../ui/safe-area-view";
+// import { SafeAreaView } from "../ui/safe-area-view";
 import { VStack } from "../ui/vstack";
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SafeAreaView className="h-full w-full">
-      <VStack className="h-full w-full bg-background-0">
-        <Header />
+    // <SafeAreaView className="h-full w-full">
+    <VStack className="h-full w-full bg-background-0">
+      <Header />
 
-        <VStack className="h-full w-full">
-          <HStack className="h-full w-full">
-            <VStack className="w-full">{children}</VStack>
-          </HStack>
-        </VStack>
-        {children}
+      <VStack className="h-full w-full">
+        <HStack className="h-full w-full">
+          <VStack className="w-full">{children}</VStack>
+        </HStack>
       </VStack>
-    </SafeAreaView>
+      {children}
+    </VStack>
+    // </SafeAreaView>
   );
 };
--- packages/ui/src/providers/index.tsx
+++ packages/ui/src/providers/index.tsx
@@ -1,24 +1,21 @@
 "use client";
 
 // react-native will select .web.tsx for web versions
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-
+// import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { GluestackUIProvider } from "./gluestack-ui-provider";
-import { SafeAreaProvider } from "./safe-area-provider";
+// import { SafeAreaProvider } from "./safe-area-provider";
 import { ThemeProvider } from "./theme-provider";
 
 export function UiProviders({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <SafeAreaProvider>
-        <GluestackUIProvider>
-          <ThemeProvider>
-            <GestureHandlerRootView style={{ flex: 1 }}>
-              {children}
-            </GestureHandlerRootView>
-          </ThemeProvider>
-        </GluestackUIProvider>
-      </SafeAreaProvider>
-    </>
+    // <SafeAreaProvider>
+    <GluestackUIProvider>
+      <ThemeProvider>
+        {/* <GestureHandlerRootView style={{ flex: 1 }}> */}
+        {children}
+        {/* </GestureHandlerRootView> */}
+      </ThemeProvider>
+    </GluestackUIProvider>
+    // </SafeAreaProvider>
   );
 }
