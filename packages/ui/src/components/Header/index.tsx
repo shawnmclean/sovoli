0ecbeda0dad1d56d736e244ab215fbf8effd3171
Shawn Mclean <shawnrmclean@gmail.com>
2024-08-31 22:30:05 -0500

    update header

--- packages/ui/src/components/Header/index.tsx
+++ packages/ui/src/components/Header/index.tsx
@@ -3,7 +3,8 @@
 import { Pressable } from "react-native";
 import { Box } from "@sovoli/ui/components/ui/box";
 
-import { Avatar, AvatarFallbackText } from "../ui/avatar";
+import { ThemeToggle } from "../ThemeToggle/ThemeToggle";
+// import { Avatar, AvatarFallbackText } from "../ui/avatar";
 import { HStack } from "../ui/hstack";
 import { ChevronLeftIcon, Icon, MenuIcon } from "../ui/icon";
 import { Text } from "../ui/text";
@@ -25,18 +26,21 @@ function MobileHeader() {
   // const router = useRouter();
   return (
     <HStack
-      className="border-border-50 items-center border-b bg-background-0 px-4 py-6"
+      className="border-border-50 items-center justify-between border-b bg-background-0 px-4 py-6"
       space="md"
     >
-      <Pressable
-        onPress={() => {
-          // router.back();
-          console.log("back");
-        }}
-      >
-        <Icon as={ChevronLeftIcon} />
-      </Pressable>
-      <Text className="text-xl">Mobile Header</Text>
+      <HStack className="items-center">
+        <Pressable
+          onPress={() => {
+            // router.back();
+            console.log("back");
+          }}
+        >
+          <Icon as={ChevronLeftIcon} className="mr-2" />
+        </Pressable>
+        <Text className="text-xl">Mobile Header</Text>
+      </HStack>
+      <ThemeToggle />
     </HStack>
   );
 }
@@ -55,9 +59,10 @@ function WebHeader() {
         <Text className="text-2xl">Web Header</Text>
       </HStack>
 
-      <Avatar className="h-9 w-9">
+      {/* <Avatar className="h-9 w-9">
         <AvatarFallbackText className="font-light">A</AvatarFallbackText>
-      </Avatar>
+      </Avatar> */}
+      <ThemeToggle />
     </HStack>
   );
 }
