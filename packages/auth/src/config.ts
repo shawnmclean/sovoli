f6f012f0259ef6f2a84c35fe9ec4a7f3e2a89556
Shawn Mclean <shawnrmclean@gmail.com>
2024-09-10 21:49:57 -0500

    differentiate between PAT and JWT

--- packages/auth/src/config.ts
+++ packages/auth/src/config.ts
@@ -3,6 +3,7 @@ import type {
   NextAuthConfig,
   Session as NextAuthSession,
 } from "next-auth";
+import { decode } from "@auth/core/jwt";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@sovoli/db";
 // import {
@@ -37,17 +38,60 @@ export const authConfig: NextAuthConfig = {
   },
 };
 
+const PAT_PREFIX = "sop_";
+/**
+ * Validate a personal access token
+ * @param token the personal access token
+ * @returns
+ */
 export const validateToken = async (
   token: string,
 ): Promise<NextAuthSession | null> => {
   console.log("validateToken", token);
-  const session = await adapter.getSessionAndUser?.(token);
-  return session
+  // validate if jwt first, if not, then validate personal access token
+  if (token.startsWith(PAT_PREFIX)) {
+    return validatePersonalAccessToken(token);
+  }
+  const jwt = await validateJwtToken(token);
+  return jwt;
+};
+
+const validateJwtToken = async (
+  token: string,
+): Promise<NextAuthSession | null> => {
+  const secureCookie = process.env.NODE_ENV === "production";
+  const decoded = await decode({
+    token,
+    secret: process.env.AUTH_SECRET ?? "secret",
+    salt: secureCookie
+      ? "__Secure-authjs.session-token"
+      : "authjs.session-token",
+  });
+
+  return decoded
     ? {
         user: {
-          ...session.user,
+          username: decoded.email,
         },
-        expires: session.session.expires.toISOString(),
+        expires: new Date().toISOString(),
+      }
+    : null;
+};
+
+const validatePersonalAccessToken = async (
+  token: string,
+): Promise<NextAuthSession | null> => {
+  // TODO: create a personal access token table and hash the token
+  // but for now, lets go with the user id as the token
+  const userToken = token.replace(PAT_PREFIX, ""); // Remove prefix
+  const user = await adapter.getUser?.(userToken);
+
+  return user
+    ? {
+        user: {
+          ...user,
+        },
+        expires: new Date().toISOString(),
       }
     : null;
 };
