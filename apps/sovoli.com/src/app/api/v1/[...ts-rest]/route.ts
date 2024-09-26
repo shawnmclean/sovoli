a184433731e2e2c9cf5c9e0f36293c3ef4ec2f11
Shawn Mclean <shawnrmclean@gmail.com>
2024-09-26 17:02:12 -0500

    move cors to handler

--- apps/sovoli.com/src/app/api/v1/[...ts-rest]/route.ts
+++ apps/sovoli.com/src/app/api/v1/[...ts-rest]/route.ts
@@ -6,6 +6,25 @@ import { fetchRequestHandler } from "@ts-rest/serverless/fetch";
 
 export const dynamic = "force-dynamic";
 
+/**
+ * Configure basic CORS headers
+ * You should extend this to match your needs
+ */
+const setCorsHeaders = (res: Response) => {
+  res.headers.set("Access-Control-Allow-Origin", "*");
+  res.headers.set("Access-Control-Request-Method", "*");
+  res.headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
+  res.headers.set("Access-Control-Allow-Headers", "*");
+};
+
+export const OPTIONS = () => {
+  const response = new Response(null, {
+    status: 204,
+  });
+  setCorsHeaders(response);
+  return response;
+};
+
 const options = {
   jsonQuery: true,
   responseValidation: true,
