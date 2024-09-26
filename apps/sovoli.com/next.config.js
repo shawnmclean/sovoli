55cada4d196480e04432f4c7945b19ca03a68fe1
Shawn Mclean <shawnrmclean@gmail.com>
2024-09-26 11:36:21 -0500

    add CORS for API

--- apps/sovoli.com/next.config.js
+++ apps/sovoli.com/next.config.js
@@ -7,6 +7,30 @@ createJiti(fileURLToPath(import.meta.url))("./src/env");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  headers: () => [
+    {
+      source: "/api/:path*",
+      headers: [
+        {
+          key: "Access-Control-Allow-Credentials",
+          value: "true",
+        },
+        {
+          key: "Access-Control-Allow-Origin",
+          value: "*",
+        },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET, POST, PUT, DELETE, OPTIONS",
+        },
+        {
+          key: "Access-Control-Allow-Headers",
+          value: "Content-Type, Authorization",
+        },
+      ],
+    },
+  ],
+
   reactStrictMode: true,
   images: {
     dangerouslyAllowSVG: true,
