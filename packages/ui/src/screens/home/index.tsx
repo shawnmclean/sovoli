5b11b1e20dd69597b9dc034cf27769043f6cd592
Shawn Mclean <shawnrmclean@gmail.com>
2024-09-27 19:50:26 -0500

    update landing

--- packages/ui/src/screens/home/index.tsx
+++ packages/ui/src/screens/home/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ScrollView } from "react-native";
-import { AddIcon, Icon } from "@sovoli/ui/components/ui/icon";
 import { Link } from "@sovoli/ui/components/ui/link";
 
 import { Text } from "../../components/ui/text";
@@ -9,11 +8,9 @@ import { Text } from "../../components/ui/text";
 export function HomeScreen() {
   return (
     <ScrollView className="mx-auto">
-      <Text className="mb-4 text-2xl font-bold">
-        Hello <Icon className="ml-2" size="lg" as={AddIcon} />
-      </Text>
-      <Link href={`/roadmap`}>
-        <Text>View Roadmap</Text>
+      <Text className="mb-4 text-2xl font-bold">The Knowledge Platform</Text>
+      <Link href="https://chatgpt.com/g/g-IO9cjdNK5-bookshelf-library-manager">
+        <Text>ChatGPT Plugin</Text>
       </Link>
     </ScrollView>
   );
