887b1e2d9cdd14c562babd18d080e0633cebcb15
Shawn Mclean <shawnrmclean@gmail.com>
2024-09-27 21:41:45 -0500

    .

--- packages/ui/src/screens/home/index.tsx
+++ packages/ui/src/screens/home/index.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { ScrollView } from "react-native";
-import { Link } from "@sovoli/ui/components/ui/link";
+
+// import { Link } from "@sovoli/ui/components/ui/link";
 
 import { Text } from "../../components/ui/text";
 
@@ -9,9 +10,9 @@ export function HomeScreen() {
   return (
     <ScrollView className="mx-auto">
       <Text className="mb-4 text-2xl font-bold">The Knowledge Platform</Text>
-      <Link href="https://chatgpt.com/g/g-IO9cjdNK5-bookshelf-library-manager">
+      {/* <Link href="https://chatgpt.com/g/g-IO9cjdNK5-bookshelf-library-manager">
         <Text>ChatGPT Plugin</Text>
-      </Link>
+      </Link> */}
     </ScrollView>
   );
 }
