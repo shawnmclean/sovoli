8919b31b411c0f925dbc2456c31383f6ac0cdd81
Shawn Mclean <shawnrmclean@gmail.com>
2024-09-29 13:52:09 -0500

    add accordion to show json

--- packages/ui/src/components/KnowledgeDetails/KnowledgeDetails.tsx
+++ packages/ui/src/components/KnowledgeDetails/KnowledgeDetails.tsx
@@ -3,8 +3,19 @@ import { View } from "react-native";
 import { KnowledgeType, MediaAssetHost } from "@sovoli/db/schema";
 import { Gallery } from "@sovoli/ui/components/Gallery";
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionContentText,
+  AccordionHeader,
+  AccordionIcon,
+  AccordionItem,
+  AccordionTitleText,
+  AccordionTrigger,
+} from "../ui/accordion";
 import { Divider } from "../ui/divider";
 import { Heading } from "../ui/heading";
+import { ChevronDownIcon, ChevronUpIcon } from "../ui/icon";
 import { Text } from "../ui/text";
 import BookDetails from "./BookDetails";
 import CollectionDetails from "./CollectionDetails";
@@ -37,6 +48,42 @@ export default function KnowledgeDetails({ knowledge }: Props) {
       <View>
         <Text>{knowledge.createdAt.toString()}</Text>
       </View>
+      <Divider />
+
+      <Accordion
+        size="md"
+        variant="filled"
+        type="single"
+        isCollapsible={true}
+        isDisabled={false}
+        className="m-5 w-[90%] border border-outline-200"
+      >
+        <AccordionItem value="a">
+          <AccordionHeader>
+            <AccordionTrigger>
+              {({ isExpanded }) => {
+                return (
+                  <>
+                    <AccordionTitleText>
+                      Open at your own risk
+                    </AccordionTitleText>
+                    {isExpanded ? (
+                      <AccordionIcon as={ChevronUpIcon} className="ml-3" />
+                    ) : (
+                      <AccordionIcon as={ChevronDownIcon} className="ml-3" />
+                    )}
+                  </>
+                );
+              }}
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent>
+            <AccordionContentText>
+              <pre>{JSON.stringify(knowledge, null, 2)}</pre>
+            </AccordionContentText>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </>
   );
 }
