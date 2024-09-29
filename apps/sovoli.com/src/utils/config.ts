1f733af6b97222745d2433b7e77e8a104f05088c
Shawn Mclean <shawnrmclean@gmail.com>
2024-09-29 09:59:00 -0500

    update site description

--- apps/sovoli.com/src/utils/config.ts
+++ apps/sovoli.com/src/utils/config.ts
@@ -1,15 +1,15 @@
 export const config = {
-  title: "Sovoli: Digitize and Organize Your Bookshelf",
+  title: "Sovoli: The Knowledge Platform",
   siteName: "Sovoli",
   description:
-    "Discover Sovoli, the platform that helps you easily digitize and organize your bookshelf. Upload photos of your books, let AI recognize and categorize them, and create a digital library that’s searchable and accessible anytime. Perfect for avid readers, researchers, and book collectors looking to manage and explore their personal book collection efficiently.",
+    "Sovoli, the platform that helps you easily digitize and organize your books, notes and research materials. Upload photos of your books, let AI recognize and categorize them, and create a digital library that’s searchable and accessible anytime. Perfect for avid readers, researchers, and book collectors looking to manage and explore their personal book collection efficiently.",
   url: "https://sovoli.com",
   images: [
     {
       url: "/images/og-image.webp",
       width: 1200,
       height: 630,
-      alt: "Sovoli: Digitize and Organize Your Bookshelf",
+      alt: "Sovoli: The Knowledge Platform",
     },
   ],
 };
