5edd698756b9ce5b7c20c80c12d1660d7a4b4cf7
Shawn Mclean <shawnrmclean@gmail.com>
2024-10-07 08:15:26 -0500

    sitemap includes user's origin knowledge

--- apps/sovoli.com/src/app/sitemap.ts
+++ apps/sovoli.com/src/app/sitemap.ts
@@ -1,5 +1,36 @@
 import type { MetadataRoute } from "next";
+import { db, eq, schema } from "@sovoli/db";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [];
+import { getBaseUrl } from "~/utils/getBaseUrl";
+
+export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = getBaseUrl();
+  const originKnowledges = await db.query.Knowledge.findMany({
+    with: {
+      User: {
+        columns: {
+          username: true,
+        },
+      },
+    },
+    columns: {
+      slug: true,
+    },
+    where: eq(schema.Knowledge.isOrigin, true),
+  });
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/users`,
+      lastModified: new Date(),
+    },
+    ...originKnowledges.map((knowledge) => ({
+      url: `${baseUrl}/${knowledge.User.username}/${knowledge.slug}`,
+      lastModified: new Date(),
+    })),
+  ];
 }
