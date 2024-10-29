f064ae077554394ad5bc41ea9b63e4f19c396125
Shawn Mclean <shawnrmclean@gmail.com>
2024-10-29 16:33:09 -0500

    swap to carousel

--- packages/ui/src/components/Gallery/index.tsx
+++ packages/ui/src/components/Gallery/index.tsx
@@ -1,42 +1,40 @@
 "use client";
 
-import { Image } from "../ui/image";
+import Image from "next/image";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
 
 export interface GalleryProps {
   images: { src: string; alt: string }[];
 }
 
 export function Gallery({ images }: GalleryProps) {
-  if (!images[0]) return null;
-
   return (
-    <div className="grid h-96 grid-cols-2 gap-4 md:h-[500px]">
-      {/* Main Image */}
-      <div className="relative col-span-2 h-full md:col-span-1">
-        <div className="relative h-full w-full">
-          <Image
-            src={images[0].src}
-            alt={images[0].alt || "Main Image"}
-            width={400}
-            height={400}
-          />
-        </div>
-      </div>
-
-      {/* Thumbnails */}
-      <div className="grid grid-cols-2 gap-4 md:grid-cols-1 md:gap-2">
-        {images.slice(1, 5).map((image, index) => (
-          <div key={index} className="relative h-24 md:h-32 lg:h-40">
-            <div className="relative h-full w-full">
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent>
+        {images.map((image, i) => (
+          <CarouselItem key={i}>
+            <div className="relative h-64 w-full p-1">
+              {" "}
+              {/* Set height for aspect ratio */}
               <Image
                 src={image.src}
-                alt={image.alt || `Thumbnail ${index + 1}`}
-                className="rounded-lg"
+                alt={image.alt || "Image"}
+                fill
+                className="rounded-lg object-cover"
               />
             </div>
-          </div>
+          </CarouselItem>
         ))}
-      </div>
-    </div>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   );
 }
