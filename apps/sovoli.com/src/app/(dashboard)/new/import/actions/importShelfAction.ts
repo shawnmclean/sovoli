3fc6a3206bb013bacb1db5190b8fef880bc8306b
Shawn Mclean <shawnrmclean@gmail.com>
2024-11-12 20:47:48 -0500

    call batch create knowledge for shelves and all its books

--- apps/sovoli.com/src/app/(dashboard)/new/import/actions/importShelfAction.ts
+++ apps/sovoli.com/src/app/(dashboard)/new/import/actions/importShelfAction.ts
@@ -1,6 +1,13 @@
 "use server";
 
 import { withZod } from "@rvf/zod";
+import {
+  BatchCreateKnowledges,
+  BatchCreateKnowledgesOptions,
+} from "@sovoli/api/services/knowledge/batchCreateKnowledges";
+import { auth } from "@sovoli/auth";
+import { db } from "@sovoli/db";
+import { KnowledgeQueryType, KnowledgeType } from "@sovoli/db/schema";
 import { z } from "zod";
 
 import { groupCSVBooksByShelves } from "../lib/groupCSVBooksByShelves";
@@ -16,20 +23,29 @@ const csvFileSchema = z.instanceof(File).refine(
 );
 const importShelfSchema = z.object({
   csvFile: csvFileSchema,
-  mapping: z.array(
-    z.object({
-      from: z.string(),
-      to: z.object({
-        id: z.string().optional(),
-        name: z.string().optional(),
+  mapping: z
+    .array(
+      z.object({
+        from: z.string(),
+        to: z.object({
+          id: z.string().optional(),
+          name: z.string().optional(),
+        }),
       }),
-    }),
-  ),
+    )
+    .optional(),
 });
 
 const validator = withZod(importShelfSchema);
 
 export async function importShelfAction(formData: FormData) {
+  const session = await auth();
+  if (!session) {
+    return {
+      errors: ["You must be logged in to import shelves"],
+    };
+  }
+
   const result = await validator.validate(formData);
 
   if (result.error) {
@@ -43,8 +59,52 @@ export async function importShelfAction(formData: FormData) {
     const file = result.data.csvFile;
     const csvContent = await file.text();
     const books = parseCSVIntoBooks(csvContent);
-    const groupedBooks = groupCSVBooksByShelves(books);
-    console.log(groupedBooks.length);
+    const shelves = groupCSVBooksByShelves(books);
+
+    const mapping = result.data.mapping;
+
+    const shelvesToUpsert =
+      mapping && mapping.length > 0
+        ? shelves.filter((shelf) => mapping.some((m) => m.from === shelf.name))
+        : shelves;
+
+    const batchCreateKnowledgesOption: BatchCreateKnowledgesOptions = {
+      authUserId: session.userId,
+      knowledges: [],
+    };
+
+    for (const shelf of shelvesToUpsert) {
+      // add the shelf as a knowledge item
+      batchCreateKnowledgesOption.knowledges.push({
+        title: shelf.name,
+        description: "Shelf created by import",
+        content: `Created by import from file: ${file.name}`,
+        type: KnowledgeType.shelf,
+        isOrigin: true,
+      });
+
+      for (const book of shelf.books) {
+        // TODO: better validations here
+        const isIsbnValid = book.isbn.length > 10;
+        const query = isIsbnValid ? book.isbn : `${book.title} ${book.author}`;
+        batchCreateKnowledgesOption.knowledges.push({
+          type: KnowledgeType.book,
+          query: query,
+          queryType: isIsbnValid
+            ? KnowledgeQueryType.isbn
+            : KnowledgeQueryType.query,
+        });
+      }
+    }
+
+    // TODO: use a transaction to ensure consistency
+    const batchCreateKnowledges = new BatchCreateKnowledges(db);
+
+    const createdKnowledges = await batchCreateKnowledges.call(
+      batchCreateKnowledgesOption,
+    );
+
+    console.log("Created knowledges:", createdKnowledges);
   } catch (e) {
     console.log(e);
   }
