1bc6d669a1fd1236a22e380480b4945b5868a66a
Shawn Mclean <shawnrmclean@gmail.com>
2024-11-14 11:43:11 -0500

    fix tailwind styles not imported

--- apps/sovoli.com/tailwind.config.ts
+++ apps/sovoli.com/tailwind.config.ts
@@ -4,8 +4,8 @@ import nextUIConfig from "@sovoli/ui/tailwind.config";
 export default {
   content: [
     ...nextUIConfig.content,
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   presets: [nextUIConfig],
 } satisfies Config;
