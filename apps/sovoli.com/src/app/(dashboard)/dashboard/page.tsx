3e762015a8deeff1bfd3f31e05c8e161f56ece79
Shawn Mclean <shawnrmclean@gmail.com>
2024-11-28 14:12:14 -0500

    remove tracing from feed page

--- apps/sovoli.com/src/app/(dashboard)/dashboard/page.tsx
+++ apps/sovoli.com/src/app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { cache } from "react";
-import { trace } from "@opentelemetry/api";
 import { FeedScreen } from "@sovoli/ui/screens/feed";
 
 import { config } from "~/utils/config";
@@ -9,20 +8,7 @@ import { getLatestKnowledges } from "./lib/getLatestKnowledges";
 export const dynamic = "force-dynamic";
 
 const retrieveLatestKnowledges = cache(async () => {
-  return await trace
-    .getTracer("nextjs-example")
-    .startActiveSpan("retrieveLatestKnowledges1", async (span) => {
-      try {
-        return await getLatestKnowledges();
-      } finally {
-        span.end();
-      }
-    });
-  // try {
-
-  // } catch {
-  //   return notFound();
-  // }
+  return await getLatestKnowledges();
 });
 
 export function generateMetadata(): Metadata {
