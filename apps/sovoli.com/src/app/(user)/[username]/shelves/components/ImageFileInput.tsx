3420339c811d37f7d38bed4535fcffe2e32bd5b1
Shawn Mclean <shawnrmclean@gmail.com>
2024-12-02 17:35:02 -0500

    add slider to image media

---
+++ apps/sovoli.com/src/app/(user)/[username]/shelves/components/ImageFileInput.tsx
@@ -0,0 +1,52 @@
+import { CloudUpload } from "lucide-react";
+import { useDropzone } from "react-dropzone";
+
+export interface ImageFileInputProps {
+  name: string;
+  onFileDropped: (file: File) => void;
+}
+
+export const ImageFileInput = ({
+  name,
+  onFileDropped,
+}: ImageFileInputProps) => {
+  const { getRootProps, getInputProps } = useDropzone({
+    multiple: false, // Only allow one file at a time
+    accept: {
+      "image/jpeg": [],
+      "image/png": [],
+    },
+    onDrop: (acceptedFiles) => {
+      if (acceptedFiles[0]) {
+        onFileDropped(acceptedFiles[0]); // Call the parent callback with the first file
+      }
+    },
+  });
+
+  return (
+    <div
+      {...getRootProps({
+        className:
+          "flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer dark:hover:bg-gray-800 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600",
+      })}
+    >
+      <input
+        {...getInputProps()}
+        title="upload file"
+        name={name}
+        id={name}
+        className="sr-only"
+      />
+      <div className="flex flex-col items-center justify-center">
+        <CloudUpload className="mb-5 h-12 w-12 text-gray-600 dark:text-gray-300" />
+        <p className="mb-2 text-lg text-gray-500 dark:text-gray-400">
+          <span className="font-semibold">Click to upload</span> or drag and
+          drop
+        </p>
+        <p className="text-xs text-gray-500 dark:text-gray-400">
+          (Only *.jpeg and *.png images will be accepted)
+        </p>
+      </div>
+    </div>
+  );
+};
--- apps/sovoli.com/src/app/(user)/[username]/shelves/components/MediaManagerDialog.tsx
+++ apps/sovoli.com/src/app/(user)/[username]/shelves/components/MediaManagerDialog.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import type { Area, Point } from "react-easy-crop";
+import { useState } from "react";
 import { Button } from "@sovoli/ui/components/ui/button";
 import {
   Modal,
@@ -7,6 +11,10 @@ import {
   ModalHeader,
   useDisclosure,
 } from "@sovoli/ui/components/ui/dialog";
+import { Slider } from "@sovoli/ui/components/ui/slider";
+import Cropper from "react-easy-crop";
+
+import { ImageFileInput } from "./ImageFileInput";
 
 export interface MediaManagerProps {
   knowledgeId: string;
@@ -20,33 +28,94 @@ export function MediaManagerDialog({
   const { isOpen, onOpenChange } = useDisclosure({
     defaultOpen: true,
   });
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
 
   const onOpenChangeHandler = () => {
     onOpenChange();
     onClosed();
   };
+
+  const onCancel = () => {
+    if (imageSrc) {
+      setImageSrc(null);
+    } else {
+      onClosed();
+    }
+  };
+
+  const handleFileDropped = (file: File) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const imageFile = event.target?.result as string;
+      setImageSrc(imageFile);
+    };
+    reader.readAsDataURL(file);
+  };
+
   return (
     <Modal isOpen={isOpen} onOpenChange={onOpenChangeHandler}>
       <ModalContent>
         {(onClose) => (
           <>
             <ModalHeader className="flex flex-col gap-1">
-              Modal Title
+              Manage Media - {knowledgeId}
             </ModalHeader>
-            <ModalBody>
-              <p>{knowledgeId}</p>
+            <ModalBody className="p-5">
+              {imageSrc ? (
+                <ImageCropper imageSrc={imageSrc} />
+              ) : (
+                <ImageFileInput
+                  name="csvFile"
+                  onFileDropped={handleFileDropped}
+                />
+              )}
             </ModalBody>
             <ModalFooter>
-              <Button color="danger" variant="light" onPress={onClose}>
-                Close
+              <Button color="danger" variant="light" onPress={onCancel}>
+                Cancel
               </Button>
               <Button color="primary" onPress={onClose}>
-                Action
+                Upload
               </Button>
             </ModalFooter>
           </>
         )}
       </ModalContent>
     </Modal>
+  );
+}
+
+function ImageCropper({ imageSrc }: { imageSrc: string }) {
+  const [crop, setCrop] = useState<Point>({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
+  const onCropComplete = (croppedArea: Area, croppedAreaPixels: Area) => {
+    console.log(croppedArea, croppedAreaPixels);
+  };
+
+  return (
+    <div>
+      <div className="relative h-48 w-full bg-gray-800 sm:h-96">
+        <Cropper
+          image={imageSrc}
+          crop={crop}
+          zoom={zoom}
+          aspect={4 / 3}
+          onCropChange={setCrop}
+          onCropComplete={onCropComplete}
+          onZoomChange={setZoom}
+        />
+      </div>
+      <div>
+        <Slider
+          size="sm"
+          aria-label="Zoom"
+          value={zoom}
+          minValue={1}
+          maxValue={3}
+          step={0.1}
+          onChange={(value) => setZoom(value as number)}
+        />
+      </div>
+    </div>
   );
 }
