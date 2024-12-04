ab70cc72a343769d2966a3ea7629579c0b4d2262
Shawn Mclean <shawnrmclean@gmail.com>
2024-12-04 16:57:13 -0500

    button press console log

--- apps/sovoli.com/src/app/test/page.tsx
+++ apps/sovoli.com/src/app/test/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@sovoli/ui/components/ui/button";
 import { useFormState } from "react-dom";
 
 import type { State } from "./action";
@@ -16,6 +17,7 @@ export default function TestPage() {
       <form method="post" action={testFormAction}>
         <input type="text" name="id" />
         <input type="submit" value="Submit" />
+        <Button onPress={() => console.log("pressed")}>Test</Button>
       </form>
       {state?.message && <p>{state.message}</p>}
     </div>
