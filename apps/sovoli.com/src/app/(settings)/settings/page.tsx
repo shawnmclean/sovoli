04313a7b3ac7733f093ae6b75fff1836efcb597e
Shawn Mclean <shawnrmclean@gmail.com>
2024-12-13 13:37:42 -0500

    add some pseudoclass in settings

--- apps/sovoli.com/src/app/(settings)/settings/page.tsx
+++ apps/sovoli.com/src/app/(settings)/settings/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { auth, signIn } from "@sovoli/auth";
 
 import { getQueryClientRsc } from "~/api/query-client";
@@ -39,6 +40,18 @@ async function SignIn() {
       <h1>Server Rendered</h1>
       <p>Name from Server: {session.user?.name}</p>
 
+      <ul>
+        <li className="flex min-w-0 items-center text-foreground/50 last:font-bold last:text-foreground">
+          <Link href="/test">
+            <span className="overflow-hidden text-ellipsis">first</span>
+          </Link>
+        </li>
+        <li className="flex min-w-0 items-center text-foreground/50 last:font-bold last:text-foreground">
+          <Link href="/asd">
+            <span className="overflow-hidden text-ellipsis">second</span>
+          </Link>
+        </li>
+      </ul>
       <pre>{JSON.stringify(session, null, 2)}</pre>
     </div>
   );
