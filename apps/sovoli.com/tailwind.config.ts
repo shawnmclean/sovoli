32bc1bba7a660e233f9b342179cf782285aed93c
Shawn Mclean <shawnrmclean@gmail.com>
2024-12-13 13:58:09 -0500

    add components to the tailwind config

--- apps/sovoli.com/tailwind.config.ts
+++ apps/sovoli.com/tailwind.config.ts
@@ -6,6 +6,7 @@ export default {
     ...nextUIConfig.content,
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   presets: [nextUIConfig],
 } satisfies Config;
