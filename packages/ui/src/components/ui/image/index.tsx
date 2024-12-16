1edf1996f98cb73856976645c834928e9dc04684
Shawn Mclean <shawnrmclean@gmail.com>
2024-12-16 10:49:39 -0500

    export only nextui image

--- packages/ui/src/components/ui/image/index.tsx
+++ packages/ui/src/components/ui/image/index.tsx
@@ -1,17 +1,1 @@
-// TODO: we can switch back to NextUI Image when this is fixed: https://github.com/nextui-org/nextui/issues/2836
-
-// import type { ImageProps as NextUIImageProps } from "@nextui-org/image";
-// import type { ImageProps as NextImageProps } from "next/image";
-// import type { FC } from "react";
-import NextImage from "next/image";
-
-// import { Image as NextUIImage } from "@nextui-org/image";
-
-// // Combine the props from both NextUIImage and NextImage
-// type CombinedImageProps = Omit<NextUIImageProps, "as"> & NextImageProps;
-
-// export const Image: FC<CombinedImageProps> = ({ ...props }) => {
-//   return <NextUIImage as={NextImage} {...props} />;
-// };
-
-export { NextImage as Image };
+export * from "@nextui-org/image";
