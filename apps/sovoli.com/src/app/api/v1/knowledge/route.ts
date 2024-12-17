d7963bb353ac68e7f95df3ed393a594d01bd14d3
Shawn Mclean <shawnrmclean@gmail.com>
2024-12-16 20:57:46 -0500

    wrap auth route better

--- apps/sovoli.com/src/app/api/v1/knowledge/route.ts
+++ apps/sovoli.com/src/app/api/v1/knowledge/route.ts
@@ -1,3 +1,4 @@
+import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { auth } from "@sovoli/auth";
 
@@ -5,33 +6,35 @@ import { createKnowledge } from "~/services/knowledge/createKnowledge";
 import { PostKnowledgeSchemaRequest } from "~/tsr/router/knowledge/knowledgeContract";
 import { getBaseUrl } from "~/utils/getBaseUrl";
 
-export const POST = auth(async (request) => {
-  const user = request.auth?.user;
+export async function POST(request: NextRequest) {
+  return auth(async (authreq) => {
+    const user = authreq.auth?.user;
 
-  if (!user?.id) {
-    throw new Error("Unauthorized");
-  }
+    if (!user?.id) {
+      throw new Error("Unauthorized");
+    }
 
-  try {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const bodyRaw = await request.json();
-    const body = PostKnowledgeSchemaRequest.parse(bodyRaw);
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const bodyRaw = await request.json();
+      const body = PostKnowledgeSchemaRequest.parse(bodyRaw);
 
-    const { knowledge, authToken } = await createKnowledge({
-      knowledge: body,
-      authUserId: user.id,
-    });
+      const { knowledge, authToken } = await createKnowledge({
+        knowledge: body,
+        authUserId: user.id,
+      });
 
-    const response = {
-      ...knowledge,
-      url: `${getBaseUrl()}/${user.username}/${knowledge.slug}`,
-      authToken,
-    };
-    return NextResponse.json(response, { status: 200 });
-  } catch {
-    return NextResponse.json(
-      { error: "Failed to create knowledge" },
-      { status: 500 },
-    );
-  }
-});
+      const response = {
+        ...knowledge,
+        url: `${getBaseUrl()}/${user.username}/${knowledge.slug}`,
+        authToken,
+      };
+      return NextResponse.json(response, { status: 200 });
+    } catch {
+      return NextResponse.json(
+        { error: "Failed to create knowledge" },
+        { status: 500 },
+      );
+    }
+  })(request, {});
+}
