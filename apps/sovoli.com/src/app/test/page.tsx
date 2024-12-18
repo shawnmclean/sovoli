6fd71162d501d9635c8ac6160994b64909e3c1ed
Shawn Mclean <shawnrmclean@gmail.com>
2024-12-18 15:59:12 -0500

    force dynamic on test page

--- apps/sovoli.com/src/app/test/page.tsx
+++ apps/sovoli.com/src/app/test/page.tsx
@@ -1,5 +1,7 @@
 import { FindBookByISBN } from "~/services/books/findBookByISBN";
 
+export const dynamic = "force-dynamic";
+
 export default async function TestPage() {
   const findBookByISBN = new FindBookByISBN();
   const { book } = await findBookByISBN.call({
