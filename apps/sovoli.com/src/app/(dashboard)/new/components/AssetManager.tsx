2b8242131946234a8fbe8b7399d2c63ec1d88b15
Shawn Mclean <shawnrmclean@gmail.com>
2025-01-02 13:04:36 -0500

    show images in carousel

--- apps/sovoli.com/src/app/(dashboard)/new/components/AssetManager.tsx
+++ apps/sovoli.com/src/app/(dashboard)/new/components/AssetManager.tsx
@@ -1,5 +1,13 @@
 import type { FileRejection } from "react-dropzone";
 import { useCallback, useState } from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@sovoli/ui/components/carousel";
+import { Image } from "@sovoli/ui/components/image";
 import { CloudUpload } from "lucide-react";
 import { useDropzone } from "react-dropzone";
 import { tv } from "tailwind-variants";
@@ -7,12 +15,16 @@ import { tv } from "tailwind-variants";
 import { useAssetFileUpload } from "~/hooks/useAssetFileUpload";
 
 const dropzoneStyles = tv({
-  base: "flex flex-col items-center justify-center w-full h-64 border-2 border-dashed rounded-lg cursor-pointer ",
+  base: "flex flex-col items-center justify-center w-full h-64 border-2 border-dashed rounded-lg",
   variants: {
-    active: {
+    isDragActive: {
       true: "border-primary",
       false:
         "border-gray-200 dark:border-gray-600 hover:border-gray-300 dark:hover:border-gray-500",
+    },
+    isFilesSelected: {
+      true: "",
+      false: "cursor-pointer",
     },
   },
 });
@@ -36,6 +48,7 @@ export const AssetManager = ({ onFileUploaded }: AssetManagerProps) => {
   );
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     multiple: true,
+    noClick: files.length > 0,
     accept: {
       "image/jpeg": [],
       "image/png": [],
@@ -48,21 +61,63 @@ export const AssetManager = ({ onFileUploaded }: AssetManagerProps) => {
       <div
         {...getRootProps({
           className: dropzoneStyles({
-            active: isDragActive,
+            isDragActive,
+            isFilesSelected: files.length > 0,
           }),
         })}
       >
         <input {...getInputProps()} title="upload file" className="sr-only" />
-        <div className="flex flex-col items-center justify-center">
-          <CloudUpload className="mb-5 h-12 w-12 text-gray-600 dark:text-gray-300" />
-          <p className="mb-2 text-lg text-gray-500 dark:text-gray-400">
-            <span className="font-semibold">Click to upload</span> or drag and
-            drop
-          </p>
-          <p className="text-xs text-gray-500 dark:text-gray-400">
-            (Only *.jpeg and *.png images will be accepted)
-          </p>
-        </div>
+        {files.length > 0 ? (
+          <Carousel className="w-full overflow-hidden rounded-lg">
+            <CarouselContent>
+              {files.map((file, i) => (
+                <CarouselItem
+                  key={i}
+                  className="flex items-center justify-center"
+                >
+                  <div className="relative h-[500px] w-[100%]">
+                    <div className="flex flex-col items-center gap-2">
+                      <div className="relative h-48 w-48 items-center rounded-lg bg-gray-200">
+                        <Image
+                          src={file.preview}
+                          alt="Preview"
+                          className="object-contain"
+                        />
+                        <button
+                          type="button"
+                          className="absolute right-2 top-2 rounded-full bg-red-500 p-2 text-white"
+                          onClick={() => removeFile(file.file)}
+                        >
+                          ✕
+                        </button>
+                      </div>
+                      <div>
+                        {file.status === "uploading" && <p>Uploading...</p>}
+                        {file.status === "success" && (
+                          <p>Uploaded successfully!</p>
+                        )}
+                        {file.status === "error" && <p>Upload failed.</p>}
+                      </div>
+                    </div>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        ) : (
+          <div className="flex flex-col items-center justify-center">
+            <CloudUpload className="mb-5 h-12 w-12 text-gray-600 dark:text-gray-300" />
+            <p className="mb-2 text-lg text-gray-500 dark:text-gray-400">
+              <span className="font-semibold">Click to upload</span> or drag and
+              drop
+            </p>
+            <p className="text-xs text-gray-500 dark:text-gray-400">
+              (Only *.jpeg and *.png images will be accepted)
+            </p>
+          </div>
+        )}
       </div>
       {fileRejections.length > 0 && (
         <div className="flex flex-col gap-2">
@@ -80,30 +135,6 @@ export const AssetManager = ({ onFileUploaded }: AssetManagerProps) => {
           })}
         </div>
       )}
-      <div className="flex flex-col gap-2">
-        {files.map((file) => (
-          <div
-            key={file.file.name}
-            className="flex flex-col items-center gap-2"
-          >
-            <div className="relative h-48 w-48 items-center rounded-lg bg-gray-200">
-              <img src={file.preview} alt="Preview" />
-              <button
-                type="button"
-                className="absolute right-2 top-2 rounded-full bg-red-500 p-2 text-white"
-                onClick={() => removeFile(file.file)}
-              >
-                ✕
-              </button>
-            </div>
-            <div>
-              {file.status === "uploading" && <p>Uploading...</p>}
-              {file.status === "success" && <p>Uploaded successfully!</p>}
-              {file.status === "error" && <p>Upload failed.</p>}
-            </div>
-          </div>
-        ))}
-      </div>
     </div>
   );
 };
--- packages/ui/src/components/carousel/index.tsx
+++ packages/ui/src/components/carousel/index.tsx
@@ -6,7 +6,8 @@ import useEmblaCarousel from "embla-carousel-react";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 import { tv } from "tailwind-variants";
 
-import { Button, ButtonProps } from "../button";
+import type { ButtonProps } from "../button";
+import { Button } from "../button";
 
 type CarouselApi = UseEmblaCarouselType[1];
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
