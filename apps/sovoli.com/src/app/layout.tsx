1b935c4edd4f14d32de434fd7983f9b4c014b88c
Shawn Mclean <shawnrmclean@gmail.com>
2025-01-04 10:43:50 -0500

    suppress hydration warnings

--- apps/sovoli.com/src/app/layout.tsx
+++ apps/sovoli.com/src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body>
         <Providers>{children}</Providers>
         <SpeedInsights />
