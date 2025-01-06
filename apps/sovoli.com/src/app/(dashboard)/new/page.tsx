595411388097775972d1ad11382d731710620ab4
Shawn Mclean <shawnrmclean@gmail.com>
2025-01-06 09:01:51 -0500

    remove default json on new note

--- apps/sovoli.com/src/app/(dashboard)/new/page.tsx
+++ apps/sovoli.com/src/app/(dashboard)/new/page.tsx
@@ -1,16 +1,9 @@
 import { NoteForm } from "./components/NoteForm";
 
 export default function NewPage() {
-  const json = `{
-    "type": "doc",
-    "content": [
-      {
-      }
-    ]
-  }`;
   return (
     <div className="mx-auto max-w-7xl p-4">
-      <NoteForm title="" description="" content={json} />
+      <NoteForm title="" description="" />
     </div>
   );
 }
