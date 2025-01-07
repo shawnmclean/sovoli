08f29174c1fd5fabeea47da858ab499c360dcdf2
Shawn Mclean <shawnrmclean@gmail.com>
2025-01-06 22:10:20 -0500

    title edit toggle

--- apps/sovoli.com/src/app/[username]/[slug]/components/title/KnowledgeTitle.tsx
+++ apps/sovoli.com/src/app/[username]/[slug]/components/title/KnowledgeTitle.tsx
@@ -1,33 +1,57 @@
-import Link from "next/link";
-import { ChevronLeftIcon } from "lucide-react";
+"use client";
+
+import { useState } from "react";
+import { Button } from "@sovoli/ui/components/button";
+import { Form } from "@sovoli/ui/components/form";
+import { Input } from "@sovoli/ui/components/input";
 import { useSession } from "next-auth/react";
 
 import { useKnowledge } from "../../context/KnowledgeContext";
-import { HeaderActions } from "../HeaderActions";
 
 export function KnowledgeTitle() {
   const knowledge = useKnowledge();
   const { data: session } = useSession();
 
+  const [isEditing, setIsEditing] = useState(false);
+
+  console.log("rendering title");
+
   return (
-    <div className="flex w-full max-w-7xl items-center justify-between border-b border-divider p-6">
-      <div className="flex items-center gap-2">
-        {knowledge.type === "shelf" && (
-          <Link
-            href={`/${knowledge.User?.username}/shelves`}
-            title="Back to shelves"
-            className="hover:text-primary-dark flex items-center text-primary"
-          >
-            <ChevronLeftIcon className="h-6 w-6" />
-          </Link>
-        )}
-        <h1 className="text-2xl font-bold">{knowledge.title}</h1>
-      </div>
-      <div className="flex gap-4">
-        {session?.userId === knowledge.User?.id && (
-          <HeaderActions knowledge={knowledge} session={session} />
-        )}
-      </div>
+    <div className="flex max-w-7xl items-center border-b border-divider p-6">
+      {isEditing ? (
+        <Form className="align-center flex w-full flex-row items-center justify-between">
+          <Input
+            variant="bordered"
+            placeholder="Title"
+            classNames={{
+              input: "text-2xl font-bold",
+            }}
+            value={knowledge.title ?? ""}
+          />
+          <div className="flex gap-2">
+            <Button variant="solid" onPress={() => setIsEditing(true)}>
+              Save
+            </Button>
+            <Button variant="bordered" onPress={() => setIsEditing(false)}>
+              Cancel
+            </Button>
+          </div>
+        </Form>
+      ) : (
+        <div className="flex w-full flex-row justify-between">
+          <h1 className="text-2xl font-bold">{knowledge.title}</h1>
+
+          {session?.userId === knowledge.User?.id && (
+            <Button
+              variant="bordered"
+              size="sm"
+              onPress={() => setIsEditing(true)}
+            >
+              Edit
+            </Button>
+          )}
+        </div>
+      )}
     </div>
   );
 }
