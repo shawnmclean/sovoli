02d09c3f7c4fe909baa0fdc16116afe03abdeaf8
Shawn Mclean <shawnrmclean@gmail.com>
2025-02-10 20:50:32 -0400

    add task list item extension

--- apps/sovoli.com/src/components/Editor/extensions.ts
+++ apps/sovoli.com/src/components/Editor/extensions.ts
@@ -1,8 +1,14 @@
 import Link from "@tiptap/extension-link";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
 import StarterKit from "@tiptap/starter-kit";
 
 export const extensions = [
   StarterKit,
+  TaskList,
+  TaskItem.configure({
+    nested: true,
+  }),
   Link.configure({
     openOnClick: false,
   }),
