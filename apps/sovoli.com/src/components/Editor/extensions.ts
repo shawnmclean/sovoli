2db4b1b67cc2f5faab3950cd20eb1e19088e1345
Shawn Mclean <shawnrmclean@gmail.com>
2025-02-13 16:22:50 -0400

    support images

--- apps/sovoli.com/src/components/Editor/extensions.ts
+++ apps/sovoli.com/src/components/Editor/extensions.ts
@@ -1,3 +1,6 @@
+import FileHandler from "@tiptap-pro/extension-file-handler";
+import Dropcursor from "@tiptap/extension-dropcursor";
+import Image from "@tiptap/extension-image";
 import Link from "@tiptap/extension-link";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
@@ -11,5 +14,55 @@ export const extensions = [
   }),
   Link.configure({
     openOnClick: false,
+  }),
+  Dropcursor,
+  Image,
+  FileHandler.configure({
+    allowedMimeTypes: ["image/png", "image/jpeg", "image/gif", "image/webp"],
+    onDrop: (currentEditor, files, pos) => {
+      files.forEach((file) => {
+        const fileReader = new FileReader();
+
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+          currentEditor
+            .chain()
+            .insertContentAt(pos, {
+              type: "image",
+              attrs: {
+                src: fileReader.result,
+              },
+            })
+            .focus()
+            .run();
+        };
+      });
+    },
+    onPaste: (currentEditor, files, htmlContent) => {
+      files.forEach((file) => {
+        if (htmlContent) {
+          // if there is htmlContent, stop manual insertion & let other extensions handle insertion via inputRule
+          // you could extract the pasted file from this url string and upload it to a server for example
+          console.log(htmlContent); // eslint-disable-line no-console
+          return false;
+        }
+
+        const fileReader = new FileReader();
+
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+          currentEditor
+            .chain()
+            .insertContentAt(currentEditor.state.selection.anchor, {
+              type: "image",
+              attrs: {
+                src: fileReader.result,
+              },
+            })
+            .focus()
+            .run();
+        };
+      });
+    },
   }),
 ];
