e3a0dfdbcd362b0ba15477373bdede4e4ae6f7a3
Shawn Mclean <shawnrmclean@gmail.com>
2025-02-18 11:11:25 -0400

    remove call for feature flag

--- apps/sovoli.com/src/components/navbar/SignUpButton.tsx
+++ apps/sovoli.com/src/components/navbar/SignUpButton.tsx
@@ -2,13 +2,11 @@
 
 import NextLink from "next/link";
 import { Button } from "@sovoli/ui/components/button";
-import { useFeatureFlagVariantKey } from "posthog-js/react";
 
 export const SignUpButton = () => {
-  const variant = useFeatureFlagVariantKey("signup-clicks-conversions");
   return (
     <Button data-attr="signup" color="primary" href="/signin" as={NextLink}>
-      {variant === "subscribe" ? "Subscribe" : "Sign In"}
+      Sign In
     </Button>
   );
 };
