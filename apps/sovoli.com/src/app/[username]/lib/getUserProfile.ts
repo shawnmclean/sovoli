6044f6ec7c1936dd48e06b887108a70791d156a0
Shawn Mclean <shawnrmclean@gmail.com>
2025-03-13 17:45:43 -0400

    cache the getUserProfile

--- apps/sovoli.com/src/app/[username]/lib/getUserProfile.ts
+++ apps/sovoli.com/src/app/[username]/lib/getUserProfile.ts
@@ -1,22 +1,25 @@
 import { cache } from "react";
+import { unstable_cacheTag as cacheTag } from "next/cache";
 
 // preload pattern: https://nextjs.org/docs/app/building-your-application/data-fetching/fetching#using-react-cache-and-server-only-with-the-preload-pattern
 import "server-only";
 
-import { GetUserProfileByUsername } from "~/services/users/getUserProfileByUsername";
+import { bus } from "~/services/core/bus";
+import { GetUserProfileByUsernameQuery } from "~/services/users/queries/GetUserProfileByUsernameQuery";
 
 export const preload = (username: string) => {
   void getUserProfile(username);
 };
 
 export const getUserProfile = cache(async (username: string) => {
-  const getUserProfileByUsername = new GetUserProfileByUsername();
-
-  const { user } = await getUserProfileByUsername.call({
-    username,
-  });
+  "use cache";
+  const { user } = await bus.queryProcessor.execute(
+    new GetUserProfileByUsernameQuery(username),
+  );
 
   if (!user) return null;
+
+  cacheTag(username, user.username);
 
   return {
     id: user.id,
