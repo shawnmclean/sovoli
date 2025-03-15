34ada98f798ea4b42ae1e93608ce2178fd731b7a
Shawn Mclean <shawnrmclean@gmail.com>
2025-03-15 14:29:51 -0400

    remove unused import

--- apps/sovoli.com/src/app/[username]/lib/getUserProfile.ts
+++ apps/sovoli.com/src/app/[username]/lib/getUserProfile.ts
@@ -1,5 +1,5 @@
 import { cache } from "react";
-import { unstable_cacheTag as cacheTag, unstable_cache } from "next/cache";
+import { unstable_cache } from "next/cache";
 
 // preload pattern: https://nextjs.org/docs/app/building-your-application/data-fetching/fetching#using-react-cache-and-server-only-with-the-preload-pattern
 import "server-only";
