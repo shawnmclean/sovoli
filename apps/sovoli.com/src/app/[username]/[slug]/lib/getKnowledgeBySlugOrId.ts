163608fa4b6b19e0f952533aea9e8771e28464e5
Shawn Mclean <shawnrmclean@gmail.com>
2025-03-15 15:37:26 -0400

    remove unstable caching due to date serialization

--- apps/sovoli.com/src/app/[username]/[slug]/lib/getKnowledgeBySlugOrId.ts
+++ apps/sovoli.com/src/app/[username]/[slug]/lib/getKnowledgeBySlugOrId.ts
@@ -1,5 +1,4 @@
 import { cache } from "react";
-import { unstable_cache } from "next/cache";
 
 import "server-only";
 
@@ -10,31 +9,20 @@ export const preload = (username: string, slugOrId: string) => {
   void getKnowledgeBySlugOrId(username, slugOrId);
 };
 
-const getCachedKnowledgeBySlugOrId = unstable_cache(
-  async (username: string, slugOrId: string, authUserId?: string) => {
+export const getKnowledgeBySlugOrId = cache(
+  async (username: string, slugOrId: string) => {
+    const session = await auth();
+
     const getKnowledgeBySlugOrId = new GetKnowledgeBySlugOrId();
 
     const response = await getKnowledgeBySlugOrId.call({
       username,
       slugOrId,
-      authUserId,
+      authUserId: session?.userId,
     });
 
     if (!response.knowledge) return null;
 
     return response;
-  },
-  ["knowledge"],
-);
-
-export const getKnowledgeBySlugOrId = cache(
-  async (username: string, slugOrId: string) => {
-    const session = await auth();
-
-    return await getCachedKnowledgeBySlugOrId(
-      username,
-      slugOrId,
-      session?.user?.id,
-    );
   },
 );
