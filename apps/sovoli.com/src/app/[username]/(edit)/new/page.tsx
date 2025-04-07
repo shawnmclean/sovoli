cb5fe7576f2fb7d62daf3ff34e8704980701ed4b
Shawn Mclean <shawnrmclean@gmail.com>
2025-04-07 16:21:03 -0400

    setting up new structure for editing a knowledge

---
+++ apps/sovoli.com/src/app/[username]/(edit)/new/page.tsx
@@ -0,0 +1,3 @@
+export default function NewPage() {
+  return <div>New</div>;
+}
--- apps/sovoli.com/src/app/[username]/[slug]/edit/page.tsx
+++
@@ -1,9 +0,0 @@
-import { KnowledgeEdit } from "./components/KnowledgeEdit";
-
-export default function KnowledgeEditPage() {
-  return (
-    <div className="mx-auto max-w-7xl p-4">
-      <KnowledgeEdit />
-    </div>
-  );
-}
