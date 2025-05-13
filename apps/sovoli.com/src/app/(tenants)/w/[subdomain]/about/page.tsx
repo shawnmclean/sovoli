1dedad9098dd067ede70f8b524c6cecb14f9a2ed
Shawn Mclean <shawnrmclean@gmail.com>
2025-05-13 12:25:00 -0400

    setup meta

--- apps/sovoli.com/src/app/(tenants)/w/[subdomain]/about/page.tsx
+++ apps/sovoli.com/src/app/(tenants)/w/[subdomain]/about/page.tsx
@@ -1,3 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About",
+  description: "The Modern Academy private school",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function AboutPage() {
   return (
     <div className="flex flex-col items-center justify-center gap-4 py-20">
--- apps/sovoli.com/src/app/(tenants)/w/[subdomain]/layout.tsx
+++ apps/sovoli.com/src/app/(tenants)/w/[subdomain]/layout.tsx
@@ -1,9 +1,23 @@
+import type { Metadata } from "next";
+
 import { Footer } from "./components/Footer";
 import { TenantNavbar } from "./components/navbar/TenantNavbar";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "Modern Academy",
+    template: `%s | Modern Academy`,
+  },
+  description: "The Modern Academy private school",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
 interface Props {
   children: React.ReactNode;
 }
+
 export default function Layout({ children }: Props) {
   return (
     <div className="flex min-h-screen flex-col">
