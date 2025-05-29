b49aea485187fd62e5fcc4d24b9b3d876e49b0a1
Shawn Mclean <shawnrmclean@gmail.com>
2025-05-29 15:03:32 -0400

    update onboarding docs

--- apps/sovoli.com/src/app/(marketing)/docs/layout.tsx
+++ apps/sovoli.com/src/app/(marketing)/docs/layout.tsx
@@ -9,7 +9,7 @@ export default function DocsLayout({ children }: Props) {
     <div className="flex flex-1 overflow-hidden">
         <DocsSidebar />
         <main className="flex-1 overflow-y-auto p-4 md:p-8">
-              <div className="max-w-4xl mx-auto mdx-content">
+              <div className="max-w-4xl mx-auto prose">
           {children}
           </div></main>
     </div>
