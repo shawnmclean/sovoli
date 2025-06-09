b035cc7d61e863ccfdafa39a847372e1a985117a
Shawn Mclean <shawnmclean@users.noreply.github.com>
2025-06-09 18:44:43 -0400

    fix: use dynamic tenant icon

--- apps/sovoli.com/src/app/(tenants)/w/[username]/layout.tsx
+++ apps/sovoli.com/src/app/(tenants)/w/[username]/layout.tsx
@@ -27,9 +27,6 @@ export async function generateMetadata({ params }: Props) {
       template: `%s | ${website.siteName}`,
     },
     description: website.description,
-    icons: {
-      icon: "/favicon.ico",
-    },
     openGraph: {
       title: website.title,
       description: website.description,
