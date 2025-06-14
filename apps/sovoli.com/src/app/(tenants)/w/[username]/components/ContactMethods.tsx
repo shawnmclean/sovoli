8dd5f6f48400cbb11514430099c8b69c73353841
Shawn Mclean <shawnmclean@users.noreply.github.com>
2025-06-14 14:41:27 -0400

    fix(contact): mark methods component as client

--- apps/sovoli.com/src/app/(tenants)/w/[username]/components/ContactMethods.tsx
+++ apps/sovoli.com/src/app/(tenants)/w/[username]/components/ContactMethods.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { SiWhatsapp } from "@icons-pack/react-simple-icons";
 import { Accordion, AccordionItem } from "@sovoli/ui/components/accordion";
