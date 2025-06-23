5da6d9deb559bea3fc927e9d529788a4472b4eaa
Shawn Mclean <shawnrmclean@gmail.com>
2025-06-23 09:50:27 -0400

    middleware query handler no need data

--- apps/sovoli.com/src/modules/websites/services/queries/GetUsernameByDomainQuery.ts
+++ apps/sovoli.com/src/modules/websites/services/queries/GetUsernameByDomainQuery.ts
@@ -1,6 +1,6 @@
 import type { Query } from "~/services/core/Query";
 import type { QueryHandler } from "~/services/core/QueryHandler";
-import { DOMAIN_TO_USERNAME } from "~/modules/data/organisations";
+// import { DOMAIN_TO_USERNAME } from "~/modules/data/organisations";
 
 export class GetUsernameByDomainQuery implements Query<{ username?: string }> {
   resultType?: { username?: string };
@@ -15,7 +15,8 @@ export class GetUsernameByDomainQueryHandler
     query: GetUsernameByDomainQuery,
   ): Promise<{ username?: string }> {
     const domain = query.domain.toLowerCase();
-    const username = await Promise.resolve(DOMAIN_TO_USERNAME.get(domain));
+    console.log("domain", domain);
+    const username = await Promise.resolve("magy"); //await Promise.resolve(DOMAIN_TO_USERNAME.get(domain));
 
     return {
       username,
