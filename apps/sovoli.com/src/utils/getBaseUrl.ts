3d72ddc900243d4028502c1a137859a73de23bdd
Shawn Mclean <shawnrmclean@gmail.com>
2025-06-28 09:16:14 -0400

    hard code base url

--- apps/sovoli.com/src/utils/getBaseUrl.ts
+++ apps/sovoli.com/src/utils/getBaseUrl.ts
@@ -2,7 +2,7 @@ import { env } from "../env";
 
 export const getBaseUrl = () => {
   if (typeof window !== "undefined") return window.location.origin;
-  if (env.VERCEL_PROJECT_PRODUCTION_URL)
-    return `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  // this is pointing to ma.edu.gy, which is set in vercel. Multi tenancy domains is overriding our env.
+  if (env.VERCEL_PROJECT_PRODUCTION_URL) return `https://www.sovoli.com`;
   return `https://localhost:${env.PORT ?? 3000}`;
 };
