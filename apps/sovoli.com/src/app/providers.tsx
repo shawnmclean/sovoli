608fdd14e9a077a1ffbf954fceecdc98e6aff4ca
Shawn Mclean <shawnrmclean@gmail.com>
2025-07-18 12:09:15 -0400

    remove session provider from providers until we need auth

--- apps/sovoli.com/src/app/providers.tsx
+++ apps/sovoli.com/src/app/providers.tsx
@@ -2,18 +2,15 @@
 
 import { useRouter } from "next/navigation";
 import { HeroUIProvider } from "@sovoli/ui/providers";
-import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   return (
-    <SessionProvider>
-      <HeroUIProvider navigate={(href) => router.push(href)}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          {children}
-        </ThemeProvider>
-      </HeroUIProvider>
-    </SessionProvider>
+    <HeroUIProvider navigate={(href) => router.push(href)}>
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        {children}
+      </ThemeProvider>
+    </HeroUIProvider>
   );
 }
