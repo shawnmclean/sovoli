fed56316a0feb99c3e689d0497778c5901e5bf40
Shawn Mclean <shawnrmclean@gmail.com>
2025-08-23 11:19:55 -0500

    add course to fitright elementary

--- apps/sovoli.com/src/modules/data/organisations/vocational-school/guyana/fitright/academics/programs/elementary.ts
+++ apps/sovoli.com/src/modules/data/organisations/vocational-school/guyana/fitright/academics/programs/elementary.ts
@@ -24,6 +24,54 @@ export const FITRIGHT_ELEMENTARY_SEWING_PROGRAM: Program = {
   ],
   description:
     "This is a 6-week program that teaches students how to sew a skirt. It is designed for students who are new to sewing and want to learn the basics of sewing.",
+  courses: [
+    {
+      id: "fr-elementary-sewing-course-0",
+      subject: { id: "seams", name: "Seams" },
+      title: "Seams",
+      units: [
+        {
+          title: "Seams",
+          topics: ["Seams"],
+        },
+      ],
+    },
+    {
+      id: "fr-elementary-sewing-course-1",
+      subject: { id: "fabric", name: "Fabric" },
+      title: "Fabric",
+      units: [
+        {
+          title: "Fabric",
+          topics: ["Fabric", "Grain lines", "Thread"],
+        },
+      ],
+    },
+    // measure
+    {
+      id: "fr-elementary-sewing-course-2",
+      subject: { id: "measurements", name: "Measurements" },
+      title: "Measurements",
+      units: [
+        {
+          title: "Measurements",
+          topics: ["Measurements"],
+        },
+      ],
+    },
+    // cutting
+    {
+      id: "fr-elementary-sewing-course-3",
+      subject: { id: "cutting", name: "Cutting" },
+      title: "Cutting",
+      units: [
+        {
+          title: "Cutting",
+          topics: ["Cutting"],
+        },
+      ],
+    },
+  ],
   photos: BAG_WORKSHOP_PHOTOS,
   group: { ...FITRIGHT_SEWING_PROGRAM_GROUP, order: 1 },
   cycles: [FITRIGHT_ELEMENTARY_SEWING_SEPTEMBER_2025],
