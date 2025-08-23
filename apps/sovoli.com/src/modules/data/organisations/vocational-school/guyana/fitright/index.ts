443eb53e5943a01e0da1f6bcc93953831958c5c7
Shawn Mclean <shawnrmclean@gmail.com>
2025-08-23 11:10:25 -0500

    add fitright logo

--- apps/sovoli.com/src/modules/data/organisations/vocational-school/guyana/fitright/index.ts
+++ apps/sovoli.com/src/modules/data/organisations/vocational-school/guyana/fitright/index.ts
@@ -8,7 +8,7 @@ export const FITRIGHT_ORG: OrgInstance = {
   org: {
     username: ORG_USERNAME,
     name: "FitRight Academy",
-    logo: "/orgs/vocational-training/guyana/fitright/logo.png",
+    logo: "/orgs/vocational-training/guyana/fitright/logo.webp",
     isVerified: true,
     verification: {
       status: "verified",
