8735125c6d1dd635919d2b789a7bf06ade959721
Shawn Mclean <shawnrmclean@gmail.com>
2025-08-25 14:04:21 -0500

    add course to intermediate sewing

--- apps/sovoli.com/src/modules/data/organisations/vocational-school/guyana/fitright/academics/programs/intermediate.ts
+++ apps/sovoli.com/src/modules/data/organisations/vocational-school/guyana/fitright/academics/programs/intermediate.ts
@@ -24,6 +24,64 @@ export const FITRIGHT_INTERMEDIATE_SEWING_PROGRAM: Program = {
   ],
   description:
     "This is a 6-week program that teaches students how to sew a blouse. It is designed for students who are new to sewing and want to learn the basics of sewing.",
+  courses: [
+    {
+      id: "fr-intermediate-sewing-course-0",
+      subject: { id: "measurements", name: "Body Measurements" },
+      title: "Upper Body Measurements",
+      units: [
+        {
+          title: "Upper Body Measurements",
+          topics: ["Upper Body Measurements"],
+        },
+      ],
+    },
+    {
+      id: "fr-intermediate-sewing-course-1",
+      subject: { id: "sleeves", name: "Sleeves" },
+      title: "Sleeves",
+      units: [
+        {
+          title: "Sleeves",
+          topics: ["Sleeves"],
+        },
+      ],
+    },
+    {
+      id: "fr-intermediate-sewing-course-2",
+      subject: { id: "collars", name: "Collars" },
+      title: "Collars",
+      units: [
+        {
+          title: "Collars",
+          topics: ["Collars"],
+        },
+      ],
+    },
+    {
+      id: "fr-intermediate-sewing-course-3",
+      subject: { id: "facings", name: "Facings" },
+      title: "Facings",
+      units: [
+        {
+          title: "Facings",
+          topics: ["Facings"],
+        },
+      ],
+    },
+    // measure
+    {
+      id: "fr-intermediate-sewing-course-2",
+      subject: { id: "blouse", name: "Blouse/Shirt" },
+      title: "Blouse",
+      units: [
+        {
+          title: "Make a Blouse/Shirt",
+          topics: ["Blouse"],
+        },
+      ],
+    },
+  ],
   photos: INTEMEDIATE_SEWING_PHOTOS,
   group: { ...FITRIGHT_SEWING_PROGRAM_GROUP, order: 2 },
   cycles: [FITRIGHT_INTERMEDIATE_SEWING_OCTOBER_2025],
