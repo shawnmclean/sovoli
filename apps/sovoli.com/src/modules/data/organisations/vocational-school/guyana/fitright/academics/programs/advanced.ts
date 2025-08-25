89af8fff11e129d50bede8c3febe2ee40d3f3373
Shawn Mclean <shawnrmclean@gmail.com>
2025-08-25 14:06:22 -0500

    Add some stuff to advanced

--- apps/sovoli.com/src/modules/data/organisations/vocational-school/guyana/fitright/academics/programs/advanced.ts
+++ apps/sovoli.com/src/modules/data/organisations/vocational-school/guyana/fitright/academics/programs/advanced.ts
@@ -24,6 +24,53 @@ export const FITRIGHT_ADVANCED_SEWING_PROGRAM: Program = {
   ],
   description:
     "This is a 6-week program that teaches students how to sew a pants, dress and top. It is designed for students who are new to sewing and want to learn the basics of sewing.",
+  courses: [
+    {
+      id: "fr-intermediate-sewing-course-0",
+      subject: { id: "design-skills", name: "Design Skills" },
+      title: "Design Skills",
+      units: [
+        {
+          title: "Design Skills",
+          topics: ["Design Skills"],
+        },
+      ],
+    },
+    {
+      id: "fr-intermediate-sewing-course-1",
+      subject: { id: "darts", name: "Darts" },
+      title: "Darts",
+      units: [
+        {
+          title: "Darts",
+          topics: ["Darts"],
+        },
+      ],
+    },
+    {
+      id: "fr-intermediate-sewing-course-2",
+      subject: { id: "pleats", name: "Pleats" },
+      title: "Pleats",
+      units: [
+        {
+          title: "Pleats",
+          topics: ["Pleats"],
+        },
+      ],
+    },
+    // measure
+    {
+      id: "fr-intermediate-sewing-course-2",
+      subject: { id: "jumpsuit", name: "Jumpsuit/Pants" },
+      title: "Jumpsuit",
+      units: [
+        {
+          title: "Make a Jumpsuit",
+          topics: ["Jumpsuit"],
+        },
+      ],
+    },
+  ],
   photos: ADVANCED_SEWING_PHOTOS,
   group: { ...FITRIGHT_SEWING_PROGRAM_GROUP, order: 3 },
   cycles: [FITRIGHT_ADVANCED_SEWING_NOVEMBER_2025],
