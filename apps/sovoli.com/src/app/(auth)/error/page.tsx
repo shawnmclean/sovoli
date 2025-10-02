1aebadb2628c3d3bb4f8af8c9cdad19474da1da6
Shawn Mclean <shawnrmclean@gmail.com>
2025-10-02 11:27:18 -0500

    feat: add @ncdai/react-wheel-picker version 1.0.17 and remove unused error page component

--- apps/sovoli.com/src/app/(auth)/error/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ErrorPage() {
-  return (
-    <div className="mx-auto max-w-7xl p-4">
-      <h1>Error</h1>
-    </div>
-  );
-}
---
+++ packages/ui/src/components/wheel-picker/index.ts
@@ -0,0 +1,1 @@
+export * from "@ncdai/react-wheel-picker";
