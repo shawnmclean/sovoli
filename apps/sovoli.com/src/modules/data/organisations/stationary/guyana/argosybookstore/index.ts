8be0f5af35471c5994211d943a2e4d4ab6f8f86f
Shawn Mclean <shawnrmclean@gmail.com>
2025-10-22 13:14:24 -0500

    feat: add photo assets for Argosy Book Store and integrate them into organization data for enhanced visual representation

--- apps/sovoli.com/src/modules/data/organisations/stationary/guyana/argosybookstore/index.ts
+++ apps/sovoli.com/src/modules/data/organisations/stationary/guyana/argosybookstore/index.ts
@@ -1,8 +1,23 @@
 import type { OrgInstance } from "~/modules/organisations/types";
+import type { Photo } from "~/modules/core/photos/types";
 import { ORG_USERNAME } from "./constants";
 import { ARGOSY_BOOK_STORE_WEBSITE } from "./website";
 import { ARGOSY_BOOK_STORE_WORKFORCE } from "./workforce";
 import { ARGOSY_BOOK_STORE_CATALOG } from "./catalog";
+
+const ARGOSY_BOOK_STORE_PHOTOS: Photo[] = [
+  {
+    category: "default",
+    url: "https://res.cloudinary.com/dipyku9mn/image/upload/v1761156821/o/argosy/org/o/argosy/org/1.png",
+    assetId: "f1ea46a8a2cc2d34f756b6c3ffefd681",
+    publicId: "o/argosy/org/o/argosy/org/1",
+    width: 1536,
+    height: 1024,
+    format: "png",
+    bytes: 2569410,
+    version: 1761156821,
+  },
+];
 
 export const ARGOSY_BOOK_STORE_ORG: OrgInstance = {
   org: {
@@ -57,6 +72,7 @@ export const ARGOSY_BOOK_STORE_ORG: OrgInstance = {
         url: "https://www.facebook.com/profile.php?id=100063641135458",
       },
     ],
+    photos: ARGOSY_BOOK_STORE_PHOTOS,
   },
   websiteModule: ARGOSY_BOOK_STORE_WEBSITE,
   academicModule: null,
