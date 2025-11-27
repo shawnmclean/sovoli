46014c2add54407fceda5ac8c582b9b65cc09d61
Shawn Mclean <shawnrmclean@gmail.com>
2025-11-27 09:02:59 -0500

    upgrade to latest typescript and eslint packages

--- apps/sovoli.com/trigger.config.ts
+++ apps/sovoli.com/trigger.config.ts
@@ -3,4 +3,5 @@ import { defineConfig } from "@trigger.dev/sdk/v3";
 export default defineConfig({
   project: "proj_uuwjitvympkvsxbbmvkc",
   maxDuration: 60 * 5, // 5 mins
+  dirs: ["./src/trigger"],
 });
