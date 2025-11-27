0f0de9b0671974834f2da2e7b897e1d5557fc543
Shawn Mclean <shawnrmclean@gmail.com>
2025-11-27 11:59:50 -0500

    refactor: update hero component to use a plugin instance
    - Changed the default export of the hero component to utilize a plugin instance of heroui for improved functionality.
    - Ensured type safety by defining the heroPlugin variable with the appropriate return type from heroui.

--- packages/ui/src/hero.ts
+++ packages/ui/src/hero.ts
@@ -1,3 +1,6 @@
 import { heroui } from "@heroui/theme";
+
+const heroPlugin: ReturnType<typeof heroui> = heroui();
+
 // eslint-disable-next-line import/no-default-export
-export default heroui();
+export default heroPlugin;
