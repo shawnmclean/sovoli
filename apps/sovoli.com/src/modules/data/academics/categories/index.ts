bbc589456c03d0d7421b309f9a981901c0168017
Shawn <shawnmclean@users.noreply.github.com>
2026-01-21 13:17:58 -0500

    Update cosmetology beauty wellness categories (#127)

--- apps/sovoli.com/src/modules/data/academics/categories/index.ts
+++ apps/sovoli.com/src/modules/data/academics/categories/index.ts
@@ -1,13 +1,11 @@
 import academicEducationData from "./academicEducation.json";
 import beautyAestheticsPersonalCareData from "./beautyAestheticsPersonalCare.json";
 import fashionAndSewingData from "./fashionAndSewing.json";
-import healthWellnessTherapyData from "./healthWellnessTherapy.json";
 
 import type { ProgramCategoryDefinition } from "~/modules/academics/categories/types";
 import { programCategoryDefinitionSchema } from "~/modules/academics/categories/types";
 
 const categoryData = [
-  healthWellnessTherapyData,
   beautyAestheticsPersonalCareData,
   fashionAndSewingData,
   academicEducationData,
@@ -15,4 +13,3 @@ const categoryData = [
 
 export const PROGRAM_CATEGORY_TREE: ProgramCategoryDefinition[] =
   programCategoryDefinitionSchema.array().parse(categoryData.flat());
-
