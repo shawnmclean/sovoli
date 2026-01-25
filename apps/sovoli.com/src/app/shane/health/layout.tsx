c1b5a2c778908c7c15eae966bc8d094e4c0822aa
Shawn Mclean <shawnrmclean@gmail.com>
2026-01-24 20:24:13 -0500

    Add health layout component with metadata configuration
    - Created a new `layout.tsx` file for the health section, defining metadata to control indexing and following behavior for search engines.
    - Implemented a functional component that renders its children, establishing a basic layout structure for the health-related pages.

---
+++ apps/sovoli.com/src/app/shane/health/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+    },
+  },
+};
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
+
